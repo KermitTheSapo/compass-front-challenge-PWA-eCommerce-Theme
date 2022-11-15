538f362c4720d2be8d6fde69bedfe9a02d841c59
Robert <rjsf06@gmail.com>
2022-11-15 20:52:40 -0300

    :sparkles: feat: doing the categories component structure

--- src/components/Home/main/categories/categories.tsx
+++ src/components/Home/main/categories/categories.tsx
@@ -7,16 +7,16 @@ import Watches from '../../../../assets/imgs/home/main/categories/Watches.svg'
 
 export default function Categories() {
     return (
-        <S.CategoriesDiv>
+        <S.CategoriesContainer>
             <S.CategoriesTitle>Top Categories</S.CategoriesTitle>
             <S.CategoriesList>
                 <Item label={"Skincare"} icon={skinCare} alt={""} route={"/error"} />
                 <Item label={"Jewellery"} icon={Jewellery} alt={""} route={"/error"} />
                 <Item label={"Handbags"} icon={Handbags} alt={""} route={"/error"} />
                 <Item label={"Watches"} icon={Watches} alt={""} route={"/error"} />
-                <Item label={"Eyewear"} icon={skinCare} alt={""} route={"/error"} />
+                {/* <Item label={"Eyewear"} icon={skinCare} alt={""} route={"/error"} /> */}
                 {/* <Item label={"Eyewear"} icon={skinCare} alt={""} route={"/error"} /> */}
             </S.CategoriesList>
-        </S.CategoriesDiv>
+        </S.CategoriesContainer>
     )
 }
