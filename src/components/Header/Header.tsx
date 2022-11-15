df1e50adde144b1ae19d23b1becf4bfc5e643666
Robert <rjsf06@gmail.com>
2022-11-15 20:47:54 -0300

    :sparkles: feat: doing the header structure, menu hamburguer and desing responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,43 @@
 import * as S from "./HeaderStyle";
-import menu from "../../assets/imgs/home/header/menu.svg"
-import add from "../../assets/imgs/home/header/add.svg"
-import notification from "../../assets/imgs/home/header/notification.svg"
-import search from "../../assets/imgs/home/header/search.svg"
-import imgLogo from "../../assets/imgs/home/header/imgLogo.svg"
-import heart from "../../assets/imgs/home/header/heart.svg"
-import profile from "../../assets/imgs/home/header/profile.svg"
-import bag from "../../assets/imgs/home/header/bag.svg"
+import menu from "../../assets/imgs/header/menu.svg"
+import add from "../../assets/imgs/header/add.svg"
+import notification from "../../assets/imgs/header/notification.svg"
+import search from "../../assets/imgs/header/search.svg"
+import imgLogo from "../../assets/imgs/header/imgLogo.svg"
+import heart from "../../assets/imgs/header/heart.svg"
+import profile from "../../assets/imgs/header/profile.svg"
+import bag from "../../assets/imgs/header/bag.svg"
+import { useState } from "react";
 
 export default function Header() {
+
+    const [listCategory, setListCategory] = useState(false)
+
     return (
         <S.HeaderContainer>
             <S.MenuContainer>
-                <S.MenuImg src={menu} alt="" />
+                <S.MenuImg src={menu} alt="" onClick={() => listCategory === true ? setListCategory(false) : setListCategory(true)} />
                 <S.TitlePage>Home</S.TitlePage>
             </S.MenuContainer>
+            {listCategory && <S.NavList>
+                <S.ListUl>
+                    <S.LinkCategory href="/error">
+                        <S.LiCategory>Handbags</S.LiCategory>
+                    </S.LinkCategory>
+                    <S.LinkCategory href="/error">
+                        <S.LiCategory>Watches</S.LiCategory>
+                    </S.LinkCategory>
+                    <S.LinkCategory href="/error">
+                        <S.LiCategory>Skincare</S.LiCategory>
+                    </S.LinkCategory>
+                    <S.LinkCategory href="/error">
+                        <S.LiCategory>Jewellery</S.LiCategory>
+                    </S.LinkCategory>
+                    <S.LinkCategory href="/error">
+                        <S.LiCategory>Apparels</S.LiCategory>
+                    </S.LinkCategory>
+                </S.ListUl>
+            </S.NavList>}
             <S.NavCategory>
                 <S.LinkCategory href="/error">
                     <S.ImgLogo src={imgLogo} alt="" />
@@ -38,7 +61,7 @@ export default function Header() {
                 </S.UlCategory>
             </S.NavCategory>
             <S.IconsContainer>
-                <S.Input type="text" placeholder="Search for products or brands....." />
+                <S.Input type="text" placeholder="Search for products or brands..." />
                 <S.LinkCategory href="/error">
                     <S.IconImg src={heart} alt="" />
                 </S.LinkCategory>
