1883e16b11e760e2e11156fd8b15997ba796d37e
Robert <rjsf06@gmail.com>
2022-11-16 20:31:19 -0300

    :sparkles: faet: doing the sheet component

---
+++ src/components/Home/main/sheet/sheet.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+import * as S from "./sheetStyle"
+
+export default function Sheet() {
+    return (
+        <S.SheetContainer>
+            <h1>Sheet</h1>
+        </S.SheetContainer >
+    )
+}
