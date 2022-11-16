3b96009d34d558b0950b4dc83a26b5c024368190
Robert <rjsf06@gmail.com>
2022-11-16 17:07:02 -0300

    :sparkles: feat: doing the carousel

--- src/components/Home/main/Arrivals/arrivals.tsx
+++ src/components/Home/main/Arrivals/arrivals.tsx
@@ -1,9 +1,9 @@
 import * as S from "./arrivalsStyle"
 import ItemArrival from "./itemArrival/itemArrival"
 import heart from "../../../../assets/imgs/home/main/arrivals/heart.svg"
+import heartFill from "../../../../assets/imgs/home/main/arrivals/heartFill.svg"
 import grande from "../../../../assets/imgs/home/main/arrivals/grande.png"
 import boker from "../../../../assets/imgs/home/main/arrivals/boker.png"
-import Collosal from "../../../../assets/imgs/home/main/arrivals/Collosal.png"
 
 export default function Arrivals() {
     return (
@@ -15,9 +15,11 @@ export default function Arrivals() {
                 </S.LinkParagraph>
             </S.ArrivalHeader>
             <S.ArrivalList>
-                <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} />
-                <ItemArrival ImgSrc={boker} ImgAlt={""} itemName={"Boker"} heartSrc={heart} heartAlt={""} Description={"Blossom PouchBrown Bag"} Price={"$34.55"} />
-                {/* <ItemArrival ImgSrc={boker} ImgAlt={""} itemName={"Collosal"} heartSrc={heart} heartAlt={""} Description={"Tall Heels"} Price={"$40.00"} /> */}
+                <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} />
+                <ItemArrival ImgSrc={boker} ImgAlt={""} itemName={"Boker"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom"} Price={"$34.55"} />
+                <ItemArrival ImgSrc={boker} ImgAlt={""} itemName={"Collosal"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Tall Heels"} Price={"$40.00"} />
+                <ItemArrival ImgSrc={boker} ImgAlt={""} itemName={"Collosal"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Tall Heels"} Price={"$40.00"} />
+                <ItemArrival ImgSrc={boker} ImgAlt={""} itemName={"Collosal"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Tall Heels"} Price={"$40.00"} />
             </S.ArrivalList>
         </S.ArrivalsSection>
     )
