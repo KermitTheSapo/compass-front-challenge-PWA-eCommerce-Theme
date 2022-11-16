75e3086c8aaa93081016656684fee1f88aa92e69
Robert <rjsf06@gmail.com>
2022-11-16 20:29:50 -0300

    :sparkles: feat: doing the sheet modal

--- src/components/Home/main/Arrivals/arrivals.tsx
+++ src/components/Home/main/Arrivals/arrivals.tsx
@@ -4,22 +4,32 @@ import heart from "../../../../assets/imgs/home/main/arrivals/heart.svg"
 import heartFill from "../../../../assets/imgs/home/main/arrivals/heartFill.svg"
 import grande from "../../../../assets/imgs/home/main/arrivals/grande.png"
 import boker from "../../../../assets/imgs/home/main/arrivals/boker.png"
+import Sheet from "../sheet/sheet"
+import { useState } from "react"
+import keepState from "../../../../utils/keepState"
 
 export default function Arrivals() {
+    const [grandeStatus, setGrandeStatus] = useState(false)
+    // const [grandeStatus, setGrandeStatus] = keepState(grandeStatus);
+    const showConsole = () => {
+        grandeStatus === true ? setGrandeStatus(false) : setGrandeStatus(true)
+    }
+    console.log(grandeStatus)
     return (
         <S.ArrivalsSection>
             <S.ArrivalHeader>
-                <S.HeaderTitle>New Arrivals</S.HeaderTitle>
+                <S.HeaderTitle >New Arrivals</S.HeaderTitle>
                 <S.LinkParagraph href="/error">
                     <S.Paragraph>View all &gt;</S.Paragraph>
                 </S.LinkParagraph>
             </S.ArrivalHeader>
             <S.ArrivalList>
-                <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} />
-                <ItemArrival ImgSrc={boker} ImgAlt={""} itemName={"Boker"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom"} Price={"$34.55"} />
-                <ItemArrival ImgSrc={boker} ImgAlt={""} itemName={"Collosal"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Tall Heels"} Price={"$40.00"} />
-                <ItemArrival ImgSrc={boker} ImgAlt={""} itemName={"Collosal"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Tall Heels"} Price={"$40.00"} />
-                <ItemArrival ImgSrc={boker} ImgAlt={""} itemName={"Collosal"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Tall Heels"} Price={"$40.00"} />
+                <ItemArrival onClick={() => showConsole()} ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} />
+                {grandeStatus && <Sheet />}
+                <ItemArrival onClick={() => showConsole()} ImgSrc={boker} ImgAlt={""} itemName={"Boker"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom"} Price={"$34.55"} />
+                <ItemArrival onClick={() => showConsole()} ImgSrc={boker} ImgAlt={""} itemName={"Collosal"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Tall Heels"} Price={"$40.00"} />
+                <ItemArrival onClick={() => showConsole()} ImgSrc={boker} ImgAlt={""} itemName={"Collosal"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Tall Heels"} Price={"$40.00"} />
+                <ItemArrival onClick={() => showConsole()} ImgSrc={boker} ImgAlt={""} itemName={"Collosal"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Tall Heels"} Price={"$40.00"} />
             </S.ArrivalList>
         </S.ArrivalsSection>
     )
