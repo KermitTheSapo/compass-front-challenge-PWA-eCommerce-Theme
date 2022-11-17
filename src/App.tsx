b3bb4764315a918352beb59d422097d85d82791d
Robert <rjsf06@gmail.com>
2022-11-17 09:48:18 -0300

    :sparkles: feat: calling the GrandeItem component as route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Reset } from "./assets/styles/reset"
 import Handbags from "./components/category/handbags/handbags"
 import Error from "./components/error/error"
+import GrandeItem from "./components/grandeItem/grandeItem"
 import Home from "./components/Home/Home"
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/handbags" element={<Handbags />} />
+        <Route path="/grandeItem" element={<GrandeItem />} />
         <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
