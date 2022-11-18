8aec77ba8f1679847dd4ff9b4efefb4be961e067
Robert <rjsf06@gmail.com>
2022-11-18 17:11:01 -0300

    :sparkles: feat: adding the boujee, coach, remus to the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Reset } from "./assets/styles/reset"
+import Boujee from "./components/boujee/boujee"
 import Eyewear from "./components/category/eyewear/eyewear"
 import Handbags from "./components/category/handbags/handbags"
 import Jewellery from "./components/category/jewellery/jewellery"
 import Skincare from "./components/category/skincare/skincareStyle"
 import Watches from "./components/category/watches/watches"
+import Coach from "./components/coach/coach"
 import Error from "./components/error/error"
 import GrandeItem from "./components/grandeItem/grandeItem"
 import Home from "./components/Home/Home"
+import Remus from "./components/remus/remus"
 
 export default function App() {
   return (
@@ -21,6 +24,9 @@ export default function App() {
         <Route path="/skincare" element={<Skincare />} />
         <Route path="/watches" element={<Watches />} />
         <Route path="/grandeItem" element={<GrandeItem />} />
+        <Route path="/coach" element={<Coach />} />
+        <Route path="/remus" element={<Remus />} />
+        <Route path="/boujee" element={<Boujee />} />
         <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
