81be35aba0fd50a6c5566f1466f5b086aa5eced3
Robert <rjsf06@gmail.com>
2022-11-18 14:04:46 -0300

    :lipstick: feat: doing the productsStyle, removing the component in the desktop version

--- src/components/productPage/products/productsStyle.ts
+++ src/components/productPage/products/productsStyle.ts
@@ -16,5 +16,27 @@ export const Products = styled.div`
 `
 
 export const ProductImage = styled.img`
-    width: 95%;    
+    min-width: 95%;   
+    height: 100%; 
+    
 `
+
+export const DivProductImage = styled.div`
+    width: 95%;
+`
+
+export const DivSimilarProduct = styled.div`
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    background-color: var(--Grey);
+    box-shadow: 0px 0px 10px 0px #0000000F;
+    position: relative;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 50px;
+    left: 275px;
+`
+export const ImgSimilar = styled.img``
