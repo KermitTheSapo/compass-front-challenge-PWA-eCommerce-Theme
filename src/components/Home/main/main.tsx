c3fe15ff6d660889cd30985d2db4c7655d370d58
Robert <rjsf06@gmail.com>
2022-11-18 14:10:30 -0300

    :sparkles: feat: adding the navigate and change the href for the useNavigate

--- src/components/Home/main/main.tsx
+++ src/components/Home/main/main.tsx
@@ -7,13 +7,15 @@ import Brands from "./brands/brands"
 import Makeup from "./makeup/makeup"
 import Trending from "./trending/trending"
 import Navigation from "./bottom-navigation/navigation"
+import { useNavigate } from "react-router-dom"
 
 export default function Main() {
+    const navigate = useNavigate();
     return (
         <S.MainContainer>
             <S.AlertDiv>
                 <S.AlertH3>We are currently experiencing local customs clearance delays. For the latest updates, please check your order status</S.AlertH3>
-                <S.AlertLink href="/error">here</S.AlertLink>
+                <S.AlertLink onClick={() => navigate("/error")}>here</S.AlertLink>
             </S.AlertDiv>
             <S.BannersCarrossel>
                 <S.Banner src={Funk} alt="" />
@@ -24,7 +26,7 @@ export default function Main() {
                 <S.CarryDiv>
                     <S.CarryTitle>Carry your Funk</S.CarryTitle>
                     <S.CarryParagraph>Trendy handbags collection for your party animal</S.CarryParagraph>
-                    <a href="/error">
+                    <a onClick={() => navigate("/")}>
                         <S.BtnBanner>See more</S.BtnBanner>
                     </a>
                 </S.CarryDiv>
