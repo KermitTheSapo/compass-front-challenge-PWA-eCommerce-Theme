d8b28573f2f11be2e791c593eb1d92a47ba02d43
Robert <rjsf06@gmail.com>
2022-11-18 17:24:39 -0300

    :sparkles: feat: calling the product page component with the grande informations and changing the page title

--- src/components/grandeItem/grandeItem.tsx
+++ src/components/grandeItem/grandeItem.tsx
@@ -1,7 +1,14 @@
 import ProductPage from "../productPage/productPage";
+import grandeItem from "../../assets/imgs/grande/grandeItem.png"
+import { Helmet } from "react-helmet";
 
 export default function GrandeItem() {
     return (
-        <ProductPage />
+        <>
+            <ProductPage name="Grande" img={grandeItem} discount="50%OFF" productDescription="Experience comfortable and easy travelling like never before with this coach bag. It features a zip closure, removable straps and multiple organization compartments to keep your valuables safe. Crafted from premium material, it is durable and lasts long." productParagraph="Blossom Pouch" safe="$78.66" value="$39.49" imgCarousel={grandeItem} />
+            <Helmet>
+                <title>Coral'l | Grande</title>
+            </Helmet>
+        </>
     )
 }
