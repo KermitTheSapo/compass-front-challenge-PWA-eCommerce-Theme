c5d3aa532a7dfe7d23e032557bf19c50fd7b058c
Robert <rjsf06@gmail.com>
2022-11-18 17:20:10 -0300

    :sparkles: feat: changing the title and calling the category jewellery structure

--- src/components/category/jewellery/jewellery.tsx
+++ src/components/category/jewellery/jewellery.tsx
@@ -1,7 +1,13 @@
+import { Helmet } from "react-helmet"
 import Category from "../category"
 
 export default function Jewellery() {
     return (
-        <Category name={"Jewellery"} />
+        <>
+            <Category name={"Jewellery"} />
+            <Helmet>
+                <title>Coral'l | Jewellery</title>
+            </Helmet>
+        </>
     )
 }
