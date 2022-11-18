688a35cff16fbb17689c6a0727eee18af869cf45
Robert <rjsf06@gmail.com>
2022-11-18 17:26:52 -0300

    :sparkles: feat: making a props to call the img in another component

--- src/components/productPage/products/products.tsx
+++ src/components/productPage/products/products.tsx
@@ -1,24 +1,27 @@
 import * as S from "./productsStyle"
-import coach from "../../../assets/imgs/productPage/coach.png"
 import viewSimilar from "../../../assets/imgs/productPage/viewSimilar.svg"
 
-export default function Products() {
+type Props = {
+    imgCarousel: string;
+}
+
+export default function Products({ imgCarousel }: Props) {
     return (
         <S.Products>
             <S.DivProductImage>
-                <S.ProductImage src={coach} alt="" />
+                <S.ProductImage src={imgCarousel} alt="" />
                 <S.DivSimilarProduct>
                     <S.ImgSimilar src={viewSimilar} alt="" />
                 </S.DivSimilarProduct>
             </S.DivProductImage>
             <S.DivProductImage>
-                <S.ProductImage src={coach} alt="" />
+                <S.ProductImage src={imgCarousel} alt="" />
                 <S.DivSimilarProduct>
                     <S.ImgSimilar src={viewSimilar} alt="" />
                 </S.DivSimilarProduct>
             </S.DivProductImage>
             <S.DivProductImage>
-                <S.ProductImage src={coach} alt="" />
+                <S.ProductImage src={imgCarousel} alt="" />
                 <S.DivSimilarProduct>
                     <S.ImgSimilar src={viewSimilar} alt="" />
                 </S.DivSimilarProduct>
