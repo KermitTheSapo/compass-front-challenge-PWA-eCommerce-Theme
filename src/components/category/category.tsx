ad5e7abbf0e952b3d1d6ceffb899e74451cf1d25
Robert <rjsf06@gmail.com>
2022-11-18 17:18:05 -0300

    :sparkles: feat: doign the category structure

--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -14,6 +14,7 @@ import ItemArrival from "../Home/main/Arrivals/itemArrival/itemArrival"
 import { useNavigate } from "react-router-dom"
 import Header from "../Header/Header"
 import MenuSideNav from "./menuSideNav/menuSideNav"
+import { useState } from "react"
 
 type Props = {
     name: string;
@@ -21,6 +22,19 @@ type Props = {
 
 export default function Category({ name }: Props) {
     const navigate = useNavigate();
+
+    const [isGridActive, setIsGridActive] = useState(true)
+    const [isSortActive, setIsSortActive] = useState(false)
+
+    const changeValueGrid = () => {
+        setIsGridActive(true)
+        setIsSortActive(false)
+    }
+    const changeValueSort = () => {
+        setIsSortActive(true)
+        setIsGridActive(false)
+    }
+
     return (
         <S.CategoryContainer>
             <Header />
@@ -42,38 +56,40 @@ export default function Category({ name }: Props) {
             <S.CategoryDiv>
                 <MenuSideNav />
                 <S.Content>
-                    <S.GridSort>
-                        <S.GridDiv>
-                            <S.ImgGrid src={grid} alt="" />
-                        </S.GridDiv>
-                        <S.GridDiv>
-                            <S.ImgGrid src={sortImg} alt="" />
-                        </S.GridDiv>
+                    <S.ListingOptions>
+                        <S.GridSort>
+                            <S.GridDiv onClick={() => isGridActive ? setIsGridActive(false) : changeValueGrid()} color={isGridActive ? "#1B4B66" : "transparent"}>
+                                <S.ImgGrid src={grid} alt="" />
+                            </S.GridDiv>
+                            <S.GridDiv onClick={() => isSortActive ? setIsSortActive(false) : changeValueSort()} color={isSortActive ? "#1B4B66" : "transparent"}>
+                                <S.ImgGrid src={sortImg} alt="" />
+                            </S.GridDiv>
+                        </S.GridSort>
                         <S.ParagraphShow>Showing 1 - 40 of 145 items</S.ParagraphShow>
                         <S.ToShow>
-                            <h3>To Show</h3>
-                            <select name="" id="">
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>
+                            <S.ToShowTitle>To Show:</S.ToShowTitle>
+                            <S.SelectToShow name="" id="">
+                                <S.OptionToShow value="1">1</S.OptionToShow>
+                                <S.OptionToShow value="2">2</S.OptionToShow>
+                                <S.OptionToShow value="3">3</S.OptionToShow>
+                                <S.OptionToShow value="4">4</S.OptionToShow>
+                                <S.OptionToShow value="5">5</S.OptionToShow>
+                            </S.SelectToShow>
                         </S.ToShow>
                         <S.SortBy>
-                            <h3>Sort By</h3>
-                            <select name="" id="">
-                                <option value="Position">Position</option>
-                                <option value="Position">Position</option>
-                                <option value="Position">Position</option>
-                                <option value="Position">Position</option>
-                                <option value="Position">Position</option>
-                            </select>
+                            <S.SortTitle>Sort By</S.SortTitle>
+                            <S.SelectSort name="" id="">
+                                <S.OptionSort value="Position">Position</S.OptionSort>
+                                <S.OptionSort value="Position">Position</S.OptionSort>
+                                <S.OptionSort value="Position">Position</S.OptionSort>
+                                <S.OptionSort value="Position">Position</S.OptionSort>
+                                <S.OptionSort value="Position">Position</S.OptionSort>
+                            </S.SelectSort>
                         </S.SortBy>
                         <S.ProductHeader>
                             <S.ProductsCount>255 Products</S.ProductsCount>
                         </S.ProductHeader>
-                    </S.GridSort>
+                    </S.ListingOptions >
                     <S.Products>
                         <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grandeItem" />
                         <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grandeItem" />
@@ -86,8 +102,8 @@ export default function Category({ name }: Props) {
                         <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grandeItem" />
                         <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grandeItem" />
                     </S.Products>
-                </S.Content>
-            </S.CategoryDiv>
+                </S.Content >
+            </S.CategoryDiv >
             <S.FooterBar>
                 <S.BtnDiv>
                     <S.ImgIcon src={sort} alt="" />
