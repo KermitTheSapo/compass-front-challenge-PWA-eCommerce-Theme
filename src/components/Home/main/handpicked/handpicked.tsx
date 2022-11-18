8c44091de6af977db9f6a33338defd1095e2b862
Robert <rjsf06@gmail.com>
2022-11-18 14:11:03 -0300

    :sparkles: feat: adding the navigate from react-router-dom and change the href for the useNavigate

--- src/components/Home/main/handpicked/handpicked.tsx
+++ src/components/Home/main/handpicked/handpicked.tsx
@@ -4,22 +4,26 @@ import personalCare from "../../../../assets/imgs/home/handpicked/personalCare.p
 import sunglasses from "../../../../assets/imgs/home/handpicked/sunglasses.png"
 import handbags from "../../../../assets/imgs/home/handpicked/handbags.png"
 import wristWatches from "../../../../assets/imgs/home/handpicked/wristWatches.png"
+import { useNavigate } from "react-router-dom"
 
 export default function Handpicked() {
+
+    const navigate = useNavigate()
+
     return (
         <S.HandpickedContainer>
             <S.Title>Handpicked Collections</S.Title>
             <S.Collections>
-                <a href="/error">
+                <a onClick={() => navigate("/error")}>
                     <S.ImgCollections src={personalCare} alt="" />
                 </a>
-                <a href="/error">
+                <a onClick={() => navigate("/error")}>
                     <S.ImgCollections src={sunglasses} alt="" />
                 </a>
-                <a href="/error">
+                <a onClick={() => navigate("/error")}>
                     <S.ImgCollections src={handbags} alt="" />
                 </a>
-                <a href="/error">
+                <a onClick={() => navigate("/error")}>
                     <S.ImgCollections src={wristWatches} alt="" />
                 </a>
             </S.Collections>
