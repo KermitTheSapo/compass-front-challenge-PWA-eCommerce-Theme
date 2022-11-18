8dd48a2c5ba227d84944f567dc238365e6edaf1c
Robert <rjsf06@gmail.com>
2022-11-18 14:16:50 -0300

    :sparkles: feat: adding new routes to another pages component from category

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Reset } from "./assets/styles/reset"
+import Eyewear from "./components/category/eyewear/eyewear"
 import Handbags from "./components/category/handbags/handbags"
+import Jewellery from "./components/category/jewellery/jewellery"
+import Skincare from "./components/category/skincare/skincareStyle"
+import Watches from "./components/category/watches/watches"
 import Error from "./components/error/error"
 import GrandeItem from "./components/grandeItem/grandeItem"
 import Home from "./components/Home/Home"
@@ -11,7 +15,11 @@ export default function App() {
       <Reset />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/eyewear" element={<Eyewear />} />
         <Route path="/handbags" element={<Handbags />} />
+        <Route path="/jewellery" element={<Jewellery />} />
+        <Route path="/skincare" element={<Skincare />} />
+        <Route path="/watches" element={<Watches />} />
         <Route path="/grandeItem" element={<GrandeItem />} />
         <Route path="/*" element={<Error />} />
       </Routes>
