42528553c4a50ee46e38e4ae7b158e687f55be98
Robert <rjsf06@gmail.com>
2022-11-19 16:49:55 -0300

    :sparkles: feat: changing the products in the skincare category

--- src/components/category/skincare/skincare.tsx
+++ src/components/category/skincare/skincare.tsx
@@ -1,13 +1,14 @@
 import { Helmet } from "react-helmet"
 import Category from "../category"
-
+import boker from "../../../assets/imgs/home/main/arrivals/boker.png"
+import boujee from "../../../assets/imgs/boujee/boujee.png"
 export default function Skincare() {
     return (
         <>
             <Helmet>
                 <title>Coral'l | Skincare</title>
             </Helmet>
-            <Category name={"Skincare"} />
+            <Category name={"Skincare"} imgProductOne={boujee} imgAltProductOne={""} itemNameOne={"Grande"} descriptionOne={"Blossom Pouch"} priceOne={"$39.49"} linkOne={"/grande"} imgProductTwo={boker} imgAltProductTwo={""} itemNameTwo={"Boker"} descriptionTwo={"Brown Bag"} priceTwo={"$34.55"} linkTwo={"/boker"} />
         </>
     )
 }
