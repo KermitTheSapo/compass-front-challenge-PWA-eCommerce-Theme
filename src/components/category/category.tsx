32ae644a5e8a969e820372ceb63a11da0f142d8f
Robert <rjsf06@gmail.com>
2022-11-19 16:46:32 -0300

    :sparkles: feat: adding caption to images, in alt and doing the sory by bottom sheet structure

--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -9,19 +9,32 @@ import sortImg from "../../assets/imgs/category/sort.svg"
 import filter from "../../assets/imgs/category/handbags/filter.svg"
 import heart from "../../assets/imgs/home/main/arrivals/heart.svg"
 import heartFill from "../../assets/imgs/home/main/arrivals/heartFill.svg"
-import grande from "../../assets/imgs/home/main/arrivals/grande.png"
 import ItemArrival from "../Home/main/Arrivals/itemArrival/itemArrival"
 import { useNavigate } from "react-router-dom"
 import Header from "../Header/Header"
 import MenuSideNav from "./menuSideNav/menuSideNav"
 import { useState } from "react"
+import PageNumberTab from "./pageNumberTab/pageNumberTab"
 
 type Props = {
     name: string;
+    imgProductOne: string;
+    imgAltProductOne: string;
+    itemNameOne: string;
+    descriptionOne: string;
+    priceOne: string;
+    linkOne: string;
+    imgProductTwo: string;
+    imgAltProductTwo: string;
+    itemNameTwo: string;
+    descriptionTwo: string;
+    priceTwo: string;
+    linkTwo: string;
 };
 
-export default function Category({ name }: Props) {
+export default function Category({ name, imgProductOne, imgAltProductOne, itemNameOne, descriptionOne, priceOne, linkOne, imgProductTwo, imgAltProductTwo, itemNameTwo, descriptionTwo, priceTwo, linkTwo }: Props) {
     const navigate = useNavigate();
+    const [ShowBottomSheet, setShowBottomSheet] = useState(false)
 
     const [isGridActive, setIsGridActive] = useState(true)
     const [isSortActive, setIsSortActive] = useState(false)
@@ -39,16 +52,16 @@ export default function Category({ name }: Props) {
         <S.CategoryContainer>
             <Header />
             <S.Header>
-                <S.LinkArrow onClick={() => navigate("/")}><S.ImgArrow src={arrow} alt="" /></S.LinkArrow>
+                <S.LinkArrow onClick={() => navigate("/")}><S.ImgArrow src={arrow} alt="a blue left arrow" /></S.LinkArrow>
                 <S.Title>{name}</S.Title>
             </S.Header>
             <S.Poster>
-                <S.ImgPoster src={hero} alt="" />
+                <S.ImgPoster src={hero} alt="an image of a black bag with a black tag written black friday on a white background with a written title saying 70% off black friday" />
             </S.Poster>
             <S.Breadcrumb>
                 <S.BreadcrumbHorizontal>
                     <S.Label>Home</S.Label>
-                    <img src={arrowRightSmall} alt="" />
+                    <img src={arrowRightSmall} alt="a small black arrow to the right" />
                     <S.LabelPage>{name}</S.LabelPage>
                 </S.BreadcrumbHorizontal>
                 <S.Title>{name}</S.Title>
@@ -59,10 +72,10 @@ export default function Category({ name }: Props) {
                     <S.ListingOptions>
                         <S.GridSort>
                             <S.GridDiv onClick={() => isGridActive ? setIsGridActive(false) : changeValueGrid()} color={isGridActive ? "#1B4B66" : "transparent"}>
-                                <S.ImgGrid src={grid} alt="" />
+                                <S.ImgGrid src={grid} alt="a square with 6 squares inside 3x3" />
                             </S.GridDiv>
                             <S.GridDiv onClick={() => isSortActive ? setIsSortActive(false) : changeValueSort()} color={isSortActive ? "#1B4B66" : "transparent"}>
-                                <S.ImgGrid src={sortImg} alt="" />
+                                <S.ImgGrid src={sortImg} alt="a list icon a square with a small square and a rectangle" />
                             </S.GridDiv>
                         </S.GridSort>
                         <S.ParagraphShow>Showing 1 - 40 of 145 items</S.ParagraphShow>
@@ -91,29 +104,62 @@ export default function Category({ name }: Props) {
                         </S.ProductHeader>
                     </S.ListingOptions >
                     <S.Products>
-                        <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grande" />
-                        <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grande" />
-                        <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grande" />
-                        <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grande" />
-                        <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grande" />
-                        <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grande" />
-                        <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grande" />
-                        <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grande" />
-                        <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grande" />
-                        <ItemArrival ImgSrc={grande} ImgAlt={""} itemName={"Grande"} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={"Blossom Pouch"} Price={"$39.49"} link="/grande" />
+                        <ItemArrival ImgSrc={imgProductOne} ImgAlt={imgAltProductOne} itemName={itemNameOne} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={descriptionOne} Price={priceOne} link={linkOne} />
+                        <ItemArrival ImgSrc={imgProductTwo} ImgAlt={imgAltProductTwo} itemName={itemNameTwo} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={descriptionTwo} Price={priceTwo} link={linkTwo} />
+                        <ItemArrival ImgSrc={imgProductTwo} ImgAlt={imgAltProductTwo} itemName={itemNameTwo} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={descriptionTwo} Price={priceTwo} link={linkTwo} />
+                        <ItemArrival ImgSrc={imgProductTwo} ImgAlt={imgAltProductTwo} itemName={itemNameTwo} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={descriptionTwo} Price={priceTwo} link={linkTwo} />
+                        <ItemArrival ImgSrc={imgProductTwo} ImgAlt={imgAltProductTwo} itemName={itemNameTwo} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={descriptionTwo} Price={priceTwo} link={linkTwo} />
+                        <ItemArrival ImgSrc={imgProductTwo} ImgAlt={imgAltProductTwo} itemName={itemNameTwo} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={descriptionTwo} Price={priceTwo} link={linkTwo} />
+                        <ItemArrival ImgSrc={imgProductTwo} ImgAlt={imgAltProductTwo} itemName={itemNameTwo} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={descriptionTwo} Price={priceTwo} link={linkTwo} />
+                        <ItemArrival ImgSrc={imgProductTwo} ImgAlt={imgAltProductTwo} itemName={itemNameTwo} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={descriptionTwo} Price={priceTwo} link={linkTwo} />
+                        <ItemArrival ImgSrc={imgProductTwo} ImgAlt={imgAltProductTwo} itemName={itemNameTwo} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={descriptionTwo} Price={priceTwo} link={linkTwo} />
                     </S.Products>
+                    <S.NumberPageTabs>
+                        <PageNumberTab />
+                    </S.NumberPageTabs>
                 </S.Content >
             </S.CategoryDiv >
             <S.FooterBar>
-                <S.BtnDiv>
-                    <S.ImgIcon src={sort} alt="" />
+                <S.BtnDiv onClick={() => setShowBottomSheet(true)}>
+                    <S.ImgIcon src={sort} alt="a list icon with a down arrow, indicating sort" />
                     <S.FooterName>Sort</S.FooterName>
                 </S.BtnDiv>
                 <S.BtnDiv>
-                    <S.ImgIcon src={filter} alt="" />
+                    <S.ImgIcon src={filter} alt="an icon of a funnel" />
                     <S.FooterName>Filter</S.FooterName>
                 </S.BtnDiv>
             </S.FooterBar>
+            {ShowBottomSheet && <S.BottomSheet>
+                <S.BottomSheetDark onClick={() => setShowBottomSheet(false)}></S.BottomSheetDark>
+                <S.BottomSheetContent>
+                    <S.BottomSheetHeader>
+                        <S.BottomSheetTitle>Sort By</S.BottomSheetTitle>
+                        <S.Separator></S.Separator>
+                    </S.BottomSheetHeader>
+                    <S.BottomSheetBody>
+                        <S.RadioDiv>
+                            <S.InputRadio type="radio" name="sort" id="latest" />
+                            <S.LabelBottomSheet htmlFor="latest">Latest Products</S.LabelBottomSheet>
+                        </S.RadioDiv>
+                        <S.RadioDiv>
+                            <S.InputRadio type="radio" name="sort" id="priceLow" />
+                            <S.LabelBottomSheet htmlFor="priceLow">Price- Low to High</S.LabelBottomSheet>
+                        </S.RadioDiv>
+                        <S.RadioDiv>
+                            <S.InputRadio type="radio" name="sort" id="priceHigh" />
+                            <S.LabelBottomSheet htmlFor="priceHigh">Price- High to Low</S.LabelBottomSheet>
+                        </S.RadioDiv>
+                        <S.RadioDiv>
+                            <S.InputRadio type="radio" name="sort" id="popularity" />
+                            <S.LabelBottomSheet htmlFor="popularity">Popularity</S.LabelBottomSheet>
+                        </S.RadioDiv>
+                        <S.RadioDiv>
+                            <S.InputRadio type="radio" name="sort" id="customer" />
+                            <S.LabelBottomSheet htmlFor="customer">Customer Ratings</S.LabelBottomSheet>
+                        </S.RadioDiv>
+                    </S.BottomSheetBody>
+                </S.BottomSheetContent>
+            </S.BottomSheet>}
         </S.CategoryContainer >
     )
 }
