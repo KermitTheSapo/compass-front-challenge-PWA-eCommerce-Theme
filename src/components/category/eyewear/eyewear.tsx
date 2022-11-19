a3abe7b7442822cef286355d4f7419b0da21c143
Robert <rjsf06@gmail.com>
2022-11-19 16:48:30 -0300

    :sparkles: feat: changing the products in the eyewear category

--- src/components/category/eyewear/eyewear.tsx
+++ src/components/category/eyewear/eyewear.tsx
@@ -1,10 +1,11 @@
 import Category from "../category"
 import { Helmet } from "react-helmet";
-
+import grande from "../../../assets/imgs/home/main/arrivals/grande.png"
+import boker from "../../../assets/imgs/home/main/arrivals/boker.png"
 export default function Eyewear() {
     return (
         <>
-            <Category name={"Eyewear"} />
+            <Category name={"Eyewear"} imgProductOne={grande} imgAltProductOne={""} itemNameOne={"Grande"} descriptionOne={"Blossom Pouch"} priceOne={"$39.49"} linkOne={"/grande"} imgProductTwo={boker} imgAltProductTwo={""} itemNameTwo={"Boker"} descriptionTwo={"Brown Bag"} priceTwo={"$34.55"} linkTwo={"/boker"} />
             <Helmet>
                 <title>Coral'l | Eyewear</title>
             </Helmet>
