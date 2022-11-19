1eef79557c59dbee299f6be9c3725ec176d458dc
Robert <rjsf06@gmail.com>
2022-11-19 16:48:45 -0300

    :sparkles: feat: changing the products in the handbags category

--- src/components/category/handbags/handbags.tsx
+++ src/components/category/handbags/handbags.tsx
@@ -1,10 +1,11 @@
 import { Helmet } from "react-helmet"
 import Category from "../category"
-
+import boker from "../../../assets/imgs/home/main/arrivals/boker.png"
+import Coach from "../../../assets/imgs/productPage/coach.png"
 export default function Handbags() {
     return (
         <>
-            <Category name={"Handbags"} />
+            <Category name={"Handbags"} imgProductOne={Coach} imgAltProductOne={""} itemNameOne={"Coach"} descriptionOne={"Blossom Pouch"} priceOne={"$39.49"} linkOne={"/grande"} imgProductTwo={boker} imgAltProductTwo={""} itemNameTwo={"Boker"} descriptionTwo={"Brown Bag"} priceTwo={"$34.55"} linkTwo={"/boker"} />
             <Helmet>
                 <title>Coral'l | Handbags</title>
             </Helmet>
