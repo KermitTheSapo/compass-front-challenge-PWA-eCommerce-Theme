50e202f526002e2f2279059debe67002929fd77f
Robert <rjsf06@gmail.com>
2022-11-19 16:50:22 -0300

    :sparkles: feat: changing the products in the watches category

--- src/components/category/watches/watches.tsx
+++ src/components/category/watches/watches.tsx
@@ -1,13 +1,14 @@
 import { Helmet } from "react-helmet"
 import Category from "../category"
-
+import grande from "../../../assets/imgs/home/main/arrivals/grande.png"
+import boker from "../../../assets/imgs/home/main/arrivals/boker.png"
 export default function Watches() {
     return (
         <>
             <Helmet>
                 <title>Coral'l | Watches</title>
             </Helmet>
-            <Category name={"Watches"} />
+            <Category name={"Watches"} imgProductOne={grande} imgAltProductOne={""} itemNameOne={"Grande"} descriptionOne={"Blossom Pouch"} priceOne={"$39.49"} linkOne={"/grande"} imgProductTwo={boker} imgAltProductTwo={""} itemNameTwo={"Boker"} descriptionTwo={"Brown Bag"} priceTwo={"$34.55"} linkTwo={"/boker"} />
         </>
     )
 }
