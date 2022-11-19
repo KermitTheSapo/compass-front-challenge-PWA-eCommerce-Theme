296024fe37c1783923e3875f59a247b9297a8bca
Robert <rjsf06@gmail.com>
2022-11-19 16:53:55 -0300

    :sparkles: feat: changing the href for the navigate from the react-router-dom

--- src/components/productPage/ratingsReviews/ratingReviews.tsx
+++ src/components/productPage/ratingsReviews/ratingReviews.tsx
@@ -1,11 +1,13 @@
 import * as S from "./ratingReviewsStyle"
 import arrowRight from "../../../assets/imgs/productPage/arrow-right.svg"
+import { useNavigate } from "react-router-dom"
 
 export default function RatingReviews() {
+    const navigate = useNavigate()
     return (
         <S.RatingsReviews>
             <S.RatingsReviewsTitle>Ratings & Reviews</S.RatingsReviewsTitle>
-            <S.LinkRatings href=""><S.RatingArrow src={arrowRight} alt="" /></S.LinkRatings>
+            <S.LinkRatings onClick={() => navigate("/error")}><S.RatingArrow src={arrowRight} alt="" /></S.LinkRatings>
         </S.RatingsReviews>
     )
 }
