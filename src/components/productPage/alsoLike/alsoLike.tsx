ea6135187c49f8bbda4f2dfdd6cb181284aa4586
Robert <rjsf06@gmail.com>
2022-11-19 12:04:41 -0300

    :sparkles: feat: making the request as a component adding each item with its information with props

--- src/components/productPage/alsoLike/alsoLike.tsx
+++ src/components/productPage/alsoLike/alsoLike.tsx
@@ -1,16 +1,25 @@
 import AlsoLikeItem from "./alsoLikeItem/alsoLikeItem"
+
+import grande from "../../../assets/imgs/home/main/arrivals/grande.png"
+import boker from "../../../assets/imgs/home/main/arrivals/boker.png"
+import coach from "../../../assets/imgs/productPage/coach.png"
+import remus from "../../../assets/imgs/remus/remus.png"
+import boujee from "../../../assets/imgs/boujee/boujee.png"
+
+
 import * as S from "./alsoLikeStyle"
 
 export default function AlsoLike() {
+
     return (
         <S.AlsoLike>
             <S.AlsoLikeTitle>You may also like</S.AlsoLikeTitle>
             <S.AlsoLikeProductsList>
-                <AlsoLikeItem />
-                <AlsoLikeItem />
-                <AlsoLikeItem />
-                <AlsoLikeItem />
-                <AlsoLikeItem />
+                <AlsoLikeItem imgSrc={grande} imgAlt={""} itemName={"Grande"} description={"Blossom Pouch"} price={"$39.49"} link={"/grande"} />
+                <AlsoLikeItem imgSrc={boker} imgAlt={""} itemName={"Boker"} description={"Blossom"} price={"$34.55"} link={"/boker"} />
+                <AlsoLikeItem imgSrc={coach} imgAlt={""} itemName={"Coach"} description={"Leather Coach Bag"} price={"$54.69"} link={"/coach"} />
+                <AlsoLikeItem imgSrc={remus} imgAlt={""} itemName={"Remus"} description={"Brown Strap Bag"} price={"$57.00"} link={"/remus"} />
+                <AlsoLikeItem imgSrc={boujee} imgAlt={""} itemName={"Boujee"} description={"Black Bag"} price={"$56.49"} link={"/boujee"} />
             </S.AlsoLikeProductsList>
         </S.AlsoLike>
     )
