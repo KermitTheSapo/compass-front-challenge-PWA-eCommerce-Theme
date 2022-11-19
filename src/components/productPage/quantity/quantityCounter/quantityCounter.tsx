151f9ec733b620a6abca1b56660aad8e66da89bf
Robert <rjsf06@gmail.com>
2022-11-19 12:17:48 -0300

    :sparkles: feat: creating the quantity counter like a component for be used in another places

---
+++ src/components/productPage/quantity/quantityCounter/quantityCounter.tsx
@@ -0,0 +1,16 @@
+import * as S from "./quantityCounterStyle"
+import minus from "../../../../assets/imgs/productPage/minus.svg"
+import plus from "../../../../assets/imgs/productPage/plus.svg"
+import { useState } from "react"
+
+export default function QuantityCounter() {
+    const [quantity, setQuantity] = useState(1)
+
+    return (
+        <S.QuantityCounter>
+            <S.Img onClick={() => quantity > 1 ? setQuantity(quantity - 1) : setQuantity(quantity)} src={minus} alt="" />
+            <S.QuantityNumber>{quantity}</S.QuantityNumber>
+            <S.Img onClick={() => quantity < 10 ? setQuantity(quantity + 1) : setQuantity(quantity)} src={plus} alt="" />
+        </S.QuantityCounter>
+    )
+}
