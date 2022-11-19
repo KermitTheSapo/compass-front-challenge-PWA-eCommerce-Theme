ac10845c872f1ac2eb51009904b99520d2943d5a
Robert <rjsf06@gmail.com>
2022-11-19 12:05:38 -0300

    :sparkles: feat: using props so the component can be called elsewhere with other information

--- src/components/productPage/alsoLike/alsoLikeItem/alsoLikeItem.tsx
+++ src/components/productPage/alsoLike/alsoLikeItem/alsoLikeItem.tsx
@@ -1,10 +1,22 @@
 import * as S from "./alsoLikeItemStyle"
-import trending from "../../../../assets/imgs/productPage/trending.png"
+import ItemArrival from "../../../Home/main/Arrivals/itemArrival/itemArrival"
+import heart from "../../../../assets/imgs/home/main/arrivals/heart.svg"
+import heartFill from "../../../../assets/imgs/home/main/arrivals/heartFill.svg"
 
-export default function AlsoLikeItem() {
+type Props = {
+    imgSrc: string;
+    imgAlt: string;
+    itemName: string;
+    description: string;
+    price: string;
+    link: string;
+}
+
+
+export default function AlsoLikeItem({ description, imgAlt, imgSrc, itemName, link, price }: Props) {
     return (
         <S.AlsoLikeProducts>
-            <S.AlsoLikeImgs src={trending} alt="" />
+            <ItemArrival ImgSrc={imgSrc} ImgAlt={imgAlt} itemName={itemName} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={description} Price={price} link={link} />
             <S.BtnTrending>
                 <S.SpanTrending>Trending</S.SpanTrending>
             </S.BtnTrending>
