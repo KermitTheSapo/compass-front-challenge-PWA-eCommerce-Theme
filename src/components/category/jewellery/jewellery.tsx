54228f0d39c53da22d03e6aa92ed8e8019957e61
Robert <rjsf06@gmail.com>
2022-11-19 16:48:58 -0300

    :sparkles: feat: changing the products in the jewellery category

--- src/components/category/jewellery/jewellery.tsx
+++ src/components/category/jewellery/jewellery.tsx
@@ -1,10 +1,12 @@
 import { Helmet } from "react-helmet"
 import Category from "../category"
+import remus from "../../../assets/imgs/remus/remus.png"
+import boker from "../../../assets/imgs/home/main/arrivals/boker.png"
 
 export default function Jewellery() {
     return (
         <>
-            <Category name={"Jewellery"} />
+            <Category name={"Jewellery"} imgProductOne={remus} imgAltProductOne={""} itemNameOne={"remus"} descriptionOne={"Blossom Pouch"} priceOne={"$39.49"} linkOne={"/grande"} imgProductTwo={boker} imgAltProductTwo={""} itemNameTwo={"Boker"} descriptionTwo={"Brown Bag"} priceTwo={"$34.55"} linkTwo={"/boker"} />
             <Helmet>
                 <title>Coral'l | Jewellery</title>
             </Helmet>
