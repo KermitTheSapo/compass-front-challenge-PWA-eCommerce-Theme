8bb3d20dfbd13f53c3ced3d614fcfbf0647caa91
Robert <rjsf06@gmail.com>
2022-11-19 12:15:36 -0300

    :lipstick: feat: removing the style lines and put in the quantityCounterStyle.ts

--- src/components/productPage/quantity/quantityStyle.ts
+++ src/components/productPage/quantity/quantityStyle.ts
@@ -17,21 +17,3 @@ export const QuantityTitle = styled.h3`
     font-size: 1.25rem;
     color: var(--Dark);
 `
-
-export const QuantityCounter = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    border: solid 1px var(--Primary);
-    border-radius: 4px;
-    height: 30px;
-    width: 40%;
-`
-export const QuantityNumber = styled.h4`
-    font-weight: 400;
-    font-size: 0.875rem;
-    color: var(--TypeHighEmphasis);
-`
-export const Img = styled.img`
-
-`
