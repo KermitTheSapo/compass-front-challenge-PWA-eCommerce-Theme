6d7a27ff3253056a2af1f01903d96ea40e38a97d
Robert <rjsf06@gmail.com>
2022-11-21 14:29:31 -0300

    :sparkles: feat: importing the products.json and using for the productPage informations

--- src/components/boker/boker.tsx
+++ src/components/boker/boker.tsx
@@ -2,11 +2,11 @@ import { Helmet } from "react-helmet";
 import ProductPage from "../productPage/productPage";
 
 import boker from "../../assets/imgs/home/main/arrivals/boker.png"
-
+import products from "../../products/products.json"
 export default function Boker() {
     return (
         <>
-            <ProductPage name="Boker" img={boker} discount="50%OFF" productDescription="Experience comfortable and easy travelling like never before with this coach bag. It features a zip closure, removable straps and multiple organization compartments to keep your valuables safe. Crafted from premium material, it is durable and lasts long." productParagraph="Brown Bag" safe="$78.66" value="$34.55" imgCarousel={boker} />
+            <ProductPage name={products[5].name} img={boker} discount="50%OFF" productDescription="Experience comfortable and easy travelling like never before with this coach bag. It features a zip closure, removable straps and multiple organization compartments to keep your valuables safe. Crafted from premium material, it is durable and lasts long." productParagraph="Brown Bag" safe={products[5].safe} value={products[5].price} imgCarousel={boker} />
             <Helmet>
                 <title>Coral'l | Boker</title>
             </Helmet>
--- src/components/boujee/boujee.tsx
+++ src/components/boujee/boujee.tsx
@@ -1,14 +1,15 @@
 import ProductPage from "../productPage/productPage";
 import boujee from "../../assets/imgs/boujee/boujee.png"
 import { Helmet } from "react-helmet";
-
+import products from "../../products/products.json"
 export default function Boujee() {
+
     return (
         <>
             <Helmet>
-                <title>Coral'l | Remus</title>
+                <title>Coral'l | Boujee</title>
             </Helmet>
-            <ProductPage name="boujee" img={boujee} discount="50%OFF" productDescription="Experience comfortable and easy traveling like never before with this coach bag. It features a zip closure, removable straps and multiple organization compartments to keep your valuables safe. Crafted from premium material, it is durable and lasts long." productParagraph="Black Bag" safe="$78.66" value="$56.49" imgCarousel={boujee} />
+            <ProductPage name={products[3].name} img={boujee} discount="50%OFF" productDescription="Experience comfortable and easy traveling like never before with this coach bag. It features a zip closure, removable straps and multiple organization compartments to keep your valuables safe. Crafted from premium material, it is durable and lasts long." productParagraph={products[3].paragraph} safe={products[3].safe} value={products[3].price} imgCarousel={boujee} />
         </>
     )
 }
