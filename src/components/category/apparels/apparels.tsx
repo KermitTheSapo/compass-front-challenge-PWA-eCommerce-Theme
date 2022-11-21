bbeb49e20fca70b3b4007d8d43283f5e610b533d
Robert <rjsf06@gmail.com>
2022-11-21 14:30:25 -0300

    :sparkles: feat: removing some props for do a map in the category.tsx with all informations from the products.json

--- src/components/category/apparels/apparels.tsx
+++ src/components/category/apparels/apparels.tsx
@@ -1,11 +1,9 @@
 import Category from "../category"
 import { Helmet } from "react-helmet";
-import grande from "../../../assets/imgs/home/main/arrivals/grande.png"
-import boker from "../../../assets/imgs/home/main/arrivals/boker.png"
 export default function Apparels() {
     return (
         <>
-            <Category name={"Apparels"} imgProductOne={grande} imgAltProductOne={""} itemNameOne={"Grande"} descriptionOne={"Blossom Pouch"} priceOne={"$39.49"} linkOne={"/grande"} imgProductTwo={boker} imgAltProductTwo={""} itemNameTwo={"Boker"} descriptionTwo={"Brown Bag"} priceTwo={"$34.55"} linkTwo={"/boker"} />
+            <Category name={"Apparels"} />
             <Helmet>
                 <title>Coral'l | Apparels</title>
             </Helmet>
