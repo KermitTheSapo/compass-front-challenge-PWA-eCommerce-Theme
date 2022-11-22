d230879c8942509923ef3a6761e1f63ec6c47126
Robert <rjsf06@gmail.com>
2022-11-22 09:51:12 -0300

    :lipstick: feat: doing the cart style

--- src/components/Home/main/Arrivals/arrivals.tsx
+++ src/components/Home/main/Arrivals/arrivals.tsx
@@ -1,14 +1,14 @@
 import * as S from "./arrivalsStyle"
 import ItemArrival from "./itemArrival/itemArrival"
-import grande from "assets/imgs/home/main/arrivals/grande.png"
-import boker from "../../../../assets/imgs/home/main/arrivals/boker.png"
-import coach from "../../../../assets/imgs/productPage/coach.png"
-import remus from "../../../../assets/imgs/remus/remus.png"
-import boujee from "../../../../assets/imgs/boujee/boujee.png"
-import collosal from "../../../../assets/imgs/productPage/collosal.png"
+import grande from "@/assets/imgs/home/main/arrivals/grande.png"
+import boker from "@/assets/imgs/home/main/arrivals/boker.png"
+import coach from "@/assets/imgs/productPage/coach.png"
+import remus from "@/assets/imgs/remus/remus.png"
+import boujee from "@/assets/imgs/boujee/boujee.png"
+import collosal from "@/assets/imgs/productPage/collosal.png"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import BottomSheetHome from "./bottomSheetHome/bottomSheetHome"
+
 
 export default function Arrivals() {
     const [grandeStatus, setGrandeStatus] = useState(false)
@@ -28,46 +28,12 @@ export default function Arrivals() {
                 </S.LinkParagraph>
             </S.ArrivalHeader>
             <S.ArrivalList>
-                <ItemArrival onClick={() => setGrandeStatus(true)} ImgSrc={grande} ImgAlt={""} itemName={"Grande"} Description={"Blossom Pouch"} Price={"$39.49"} link={window.screen.width > 768 ? "/grande" : ""} />
-                {grandeStatus && <>
-                    <S.BottomSheetHomeDark onClick={() => setGrandeStatus(false)}></S.BottomSheetHomeDark>
-                    <BottomSheetHome name={"Grande"} description={"Blossom Pouch"} price={"$39.49"} img={grande} link="grande" />
-                </>}
-                <ItemArrival onClick={() => setBokerStatus(true)} ImgSrc={boker} ImgAlt={""} itemName={"Boker"} Description={"Blossom"} Price={"$34.55"} link={window.screen.width > 768 ? "/boker" : ""} />
-                {bokerStatus && <>
-                    <S.BottomSheetHomeDark onClick={() => setBokerStatus(false)}></S.BottomSheetHomeDark>
-                    <BottomSheetHome name={"Boker"} description={"Blossom"} price={"$34.55"} img={boker} link="boker" />
-                </>}
-                <ItemArrival onClick={() => setCoachStatus(true)} ImgSrc={coach} ImgAlt={""} itemName={"Coach"} Description={"Leather Coach Bag"} Price={"$54.69"} link={window.screen.width > 768 ? "/coach" : ""} />
-                {coachStatus && <>
-                    <S.BottomSheetHomeDark onClick={() => setCoachStatus(false)}></S.BottomSheetHomeDark>
-                    <BottomSheetHome name={"Coach"} description={"Leather Coach Bag"} price={"$54.69"} img={coach} link="coach" />
-                </>}
-                <ItemArrival onClick={() => setRemusStatus(true)} ImgSrc={remus} ImgAlt={""} itemName={"Remus"} Description={"Brown Strap Bag"} Price={"$57.00"} link={window.screen.width > 768 ? "/remus" : ""} />
-                {remusStatus && <>
-                    <S.BottomSheetHomeDark onClick={() => setRemusStatus(false)}></S.BottomSheetHomeDark>
-                    <BottomSheetHome name={"Remus"} description={"Brown Strap Bag"} price={"$57.00"} img={remus} link="remus" />
-                </>}
-                <ItemArrival onClick={() => setCollosalStatus(true)} ImgSrc={collosal} ImgAlt={""} itemName={"Collosal"} Description={"Tall Heels"} Price={"$40.00"} link={window.screen.width > 768 ? "/collosal" : ""} />
-                {collosalStatus && <>
-                    <S.BottomSheetHomeDark onClick={() => setCollosalStatus(false)}></S.BottomSheetHomeDark>
-                    <BottomSheetHome name={"collosal"} description={"Tall Heels"} price={"$40.00"} img={collosal} link="collosal" />
-                </>}
-                <ItemArrival onClick={() => setBoujeeStatus(true)} ImgSrc={boujee} ImgAlt={""} itemName={"boujee"} Description={"Black Bag"} Price={"$56.49"} link={window.screen.width > 768 ? "/boujee" : ""} />
-                {boujeeStatus && <>
-                    <S.BottomSheetHomeDark onClick={() => setBoujeeStatus(false)}></S.BottomSheetHomeDark>
-                    <BottomSheetHome name={"boujee"} description={"Black Bag"} price={"$56.49"} img={boujee} link="boujee" />
-                </>}
-                <ItemArrival onClick={() => setBoujeeStatus(true)} ImgSrc={boujee} ImgAlt={""} itemName={"boujee"} Description={"Black Bag"} Price={"$56.49"} link={window.screen.width > 768 ? "/boujee" : ""} />
-                {boujeeStatus && <>
-                    <S.BottomSheetHomeDark onClick={() => setBoujeeStatus(false)}></S.BottomSheetHomeDark>
-                    <BottomSheetHome name={"boujee"} description={"Black Bag"} price={"$56.49"} img={boujee} link="boujee" />
-                </>}
-                <ItemArrival onClick={() => setBoujeeStatus(true)} ImgSrc={boujee} ImgAlt={""} itemName={"boujee"} Description={"Black Bag"} Price={"$56.49"} link={window.screen.width > 768 ? "/boujee" : ""} />
-                {boujeeStatus && <>
-                    <S.BottomSheetHomeDark onClick={() => setBoujeeStatus(false)}></S.BottomSheetHomeDark>
-                    <BottomSheetHome name={"boujee"} description={"Black Bag"} price={"$56.49"} img={boujee} link="boujee" />
-                </>}
+                <ItemArrival onClick={() => setGrandeStatus(true)} state={grandeStatus} ImgSrc={grande} ImgAlt={""} itemName={"Grande"} Description={"Blossom Pouch"} Price={"$39.49"} link={"/grande"} setState={() => setGrandeStatus(false)} link2="/" />
+                <ItemArrival onClick={() => setBokerStatus(true)} ImgSrc={boker} ImgAlt={""} itemName={"Boker"} Description={"Blossom"} Price={"$34.55"} link={"/boker"} setState={() => setBokerStatus(false)} state={bokerStatus} link2="/" />
+                <ItemArrival onClick={() => setCoachStatus(true)} ImgSrc={coach} ImgAlt={""} itemName={"Coach"} Description={"Leather Coach Bag"} Price={"$54.69"} link={"/coach"} setState={() => setCoachStatus(false)} state={coachStatus} link2="/" />
+                <ItemArrival onClick={() => setRemusStatus(true)} ImgSrc={remus} ImgAlt={""} itemName={"Remus"} Description={"Brown Strap Bag"} Price={"$57.00"} link={"/remus"} setState={() => setRemusStatus(false)} state={remusStatus} link2="/" />
+                <ItemArrival onClick={() => setCollosalStatus(true)} ImgSrc={collosal} ImgAlt={""} itemName={"Collosal"} Description={"Tall Heels"} Price={"$40.00"} link={"/collos"} setState={() => setCollosalStatus(false)} state={collosalStatus} link2="/" />
+                <ItemArrival onClick={() => setBoujeeStatus(true)} ImgSrc={boujee} ImgAlt={""} itemName={"boujee"} Description={"Black Bag"} Price={"$56.49"} link={"/boujee"} setState={() => setBoujeeStatus(false)} state={boujeeStatus} link2="/" />
             </S.ArrivalList>
         </S.ArrivalsSection>
     )
