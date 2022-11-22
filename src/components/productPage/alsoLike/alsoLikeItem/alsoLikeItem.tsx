b85c4b5f5d6516498afadcba1c3e02717c7033c4
Robert <rjsf06@gmail.com>
2022-11-22 10:02:51 -0300

    :sparkles: feat: making the products map with the item arrival

--- src/components/productPage/alsoLike/alsoLikeItem/alsoLikeItem.tsx
+++ src/components/productPage/alsoLike/alsoLikeItem/alsoLikeItem.tsx
@@ -1,25 +1,18 @@
 import * as S from "./alsoLikeItemStyle"
 import ItemArrival from "../../../Home/main/Arrivals/itemArrival/itemArrival"
-import heart from "../../../../assets/imgs/home/main/arrivals/heart.svg"
-import heartFill from "../../../../assets/imgs/home/main/arrivals/heartFill.svg"
+import ProductsItem from "../../../../products/products.json"
 
-type Props = {
-    imgSrc: string;
-    imgAlt: string;
-    itemName: string;
-    description: string;
-    price: string;
-    link: string;
-}
-
-
-export default function AlsoLikeItem({ description, imgAlt, imgSrc, itemName, link, price }: Props) {
+export default function AlsoLikeItem() {
     return (
         <S.AlsoLikeProducts>
-            <ItemArrival ImgSrc={imgSrc} ImgAlt={imgAlt} itemName={itemName} heartSrc={heart} heartFill={heartFill} heartAlt={""} Description={description} Price={price} link={link} />
-            <S.BtnTrending>
-                <S.SpanTrending>Trending</S.SpanTrending>
-            </S.BtnTrending>
+            {ProductsItem.map((item) => (
+                <S.Div>
+                    <ItemArrival ImgSrc={item.img} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item.link} />
+                    <S.BtnTrending>
+                        <S.SpanTrending>Trending</S.SpanTrending>
+                    </S.BtnTrending>
+                </S.Div>
+            ))}
         </S.AlsoLikeProducts>
     )
 }
