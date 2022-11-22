1597f6af9ffeacf8c9a460cb3b6d06a8ab61890e
Robert <rjsf06@gmail.com>
2022-11-22 10:01:33 -0300

    :sparkles: feat: chaging the alsoLikeTitle

--- src/components/productPage/alsoLike/alsoLike.tsx
+++ src/components/productPage/alsoLike/alsoLike.tsx
@@ -1,25 +1,11 @@
 import AlsoLikeItem from "./alsoLikeItem/alsoLikeItem"
-
-import grande from "../../../assets/imgs/home/main/arrivals/grande.png"
-import boker from "../../../assets/imgs/home/main/arrivals/boker.png"
-import coach from "../../../assets/imgs/productPage/coach.png"
-import remus from "../../../assets/imgs/remus/remus.png"
-import boujee from "../../../assets/imgs/boujee/boujee.png"
-
-
 import * as S from "./alsoLikeStyle"
-
 export default function AlsoLike() {
-
     return (
         <S.AlsoLike>
-            <S.AlsoLikeTitle>You may also like</S.AlsoLikeTitle>
+            <S.AlsoLikeTitle>You Might Also Like</S.AlsoLikeTitle>
             <S.AlsoLikeProductsList>
-                <AlsoLikeItem imgSrc={grande} imgAlt={""} itemName={"Grande"} description={"Blossom Pouch"} price={"$39.49"} link={"/grande"} />
-                <AlsoLikeItem imgSrc={boker} imgAlt={""} itemName={"Boker"} description={"Blossom"} price={"$34.55"} link={"/boker"} />
-                <AlsoLikeItem imgSrc={coach} imgAlt={""} itemName={"Coach"} description={"Leather Coach Bag"} price={"$54.69"} link={"/coach"} />
-                <AlsoLikeItem imgSrc={remus} imgAlt={""} itemName={"Remus"} description={"Brown Strap Bag"} price={"$57.00"} link={"/remus"} />
-                <AlsoLikeItem imgSrc={boujee} imgAlt={""} itemName={"Boujee"} description={"Black Bag"} price={"$56.49"} link={"/boujee"} />
+                <AlsoLikeItem />
             </S.AlsoLikeProductsList>
         </S.AlsoLike>
     )
