eb5ed1e3258defc0c044c31dc31ba61b8ea1723e
Robert <rjsf06@gmail.com>
2022-11-22 16:32:07 -0300

    :sparkles: feat: changing the price value to number without the $

--- src/components/Home/main/Arrivals/arrivals.tsx
+++ src/components/Home/main/Arrivals/arrivals.tsx
@@ -28,12 +28,12 @@ export default function Arrivals() {
                 </S.LinkParagraph>
             </S.ArrivalHeader>
             <S.ArrivalList>
-                <ItemArrival onClick={() => setGrandeStatus(true)} state={grandeStatus} ImgSrc={grande} ImgAlt={""} itemName={"Grande"} Description={"Blossom Pouch"} Price={"$39.49"} link={"/grande"} setState={() => setGrandeStatus(false)} link2="/" />
-                <ItemArrival onClick={() => setBokerStatus(true)} ImgSrc={boker} ImgAlt={""} itemName={"Boker"} Description={"Blossom"} Price={"$34.55"} link={"/boker"} setState={() => setBokerStatus(false)} state={bokerStatus} link2="/" />
-                <ItemArrival onClick={() => setCoachStatus(true)} ImgSrc={coach} ImgAlt={""} itemName={"Coach"} Description={"Leather Coach Bag"} Price={"$54.69"} link={"/coach"} setState={() => setCoachStatus(false)} state={coachStatus} link2="/" />
-                <ItemArrival onClick={() => setRemusStatus(true)} ImgSrc={remus} ImgAlt={""} itemName={"Remus"} Description={"Brown Strap Bag"} Price={"$57.00"} link={"/remus"} setState={() => setRemusStatus(false)} state={remusStatus} link2="/" />
-                <ItemArrival onClick={() => setCollosalStatus(true)} ImgSrc={collosal} ImgAlt={""} itemName={"Collosal"} Description={"Tall Heels"} Price={"$40.00"} link={"/collos"} setState={() => setCollosalStatus(false)} state={collosalStatus} link2="/" />
-                <ItemArrival onClick={() => setBoujeeStatus(true)} ImgSrc={boujee} ImgAlt={""} itemName={"boujee"} Description={"Black Bag"} Price={"$56.49"} link={"/boujee"} setState={() => setBoujeeStatus(false)} state={boujeeStatus} link2="/" />
+                <ItemArrival onClick={() => setGrandeStatus(true)} state={grandeStatus} ImgSrc={grande} ImgAlt={""} itemName={"Grande"} Description={"Blossom Pouch"} Price={39.49} link={"/grande"} setState={() => setGrandeStatus(false)} link2="/" />
+                <ItemArrival onClick={() => setBokerStatus(true)} ImgSrc={boker} ImgAlt={""} itemName={"Boker"} Description={"Blossom"} Price={34.55} link={"/boker"} setState={() => setBokerStatus(false)} state={bokerStatus} link2="/" />
+                <ItemArrival onClick={() => setCoachStatus(true)} ImgSrc={coach} ImgAlt={""} itemName={"Coach"} Description={"Leather Coach Bag"} Price={54.69} link={"/coach"} setState={() => setCoachStatus(false)} state={coachStatus} link2="/" />
+                <ItemArrival onClick={() => setRemusStatus(true)} ImgSrc={remus} ImgAlt={""} itemName={"Remus"} Description={"Brown Strap Bag"} Price={57.00} link={"/remus"} setState={() => setRemusStatus(false)} state={remusStatus} link2="/" />
+                <ItemArrival onClick={() => setCollosalStatus(true)} ImgSrc={collosal} ImgAlt={""} itemName={"Collosal"} Description={"Tall Heels"} Price={40.00} link={"/collos"} setState={() => setCollosalStatus(false)} state={collosalStatus} link2="/" />
+                <ItemArrival onClick={() => setBoujeeStatus(true)} ImgSrc={boujee} ImgAlt={""} itemName={"boujee"} Description={"Black Bag"} Price={56.49} link={"/boujee"} setState={() => setBoujeeStatus(false)} state={boujeeStatus} link2="/" />
             </S.ArrivalList>
         </S.ArrivalsSection>
     )
