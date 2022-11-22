4202569e18576a3260fd2ab31031f1dfd49c72b8
Robert <rjsf06@gmail.com>
2022-11-22 09:53:29 -0300

    :sparkles: feat: changing the routes for images from absolute to relative and putting the values in the button add component props

--- src/components/Home/main/Arrivals/bottomSheetHome/bottomSheetHome.tsx
+++ src/components/Home/main/Arrivals/bottomSheetHome/bottomSheetHome.tsx
@@ -1,8 +1,8 @@
 import * as S from "./bottomSheetHomeStyle"
 
-import star from "../../../../../assets/imgs/home/main/arrivals/star.png"
-import arrow from "../../../../../assets/imgs/home/main/arrivals/arrowLeft.svg"
-import placeholder from "../../../../../assets/imgs/home/main/arrivals/placeholder.svg"
+import star from "@/assets/imgs/home/main/arrivals/star.png"
+import arrow from "@/assets/imgs/home/main/arrivals/arrowLeft.svg"
+import placeholder from "@/assets/imgs/home/main/arrivals/placeholder.svg"
 import ButtonAdd from "./buttonAdd/buttonAdd"
 import { useNavigate } from "react-router-dom"
 
@@ -12,46 +12,50 @@ type Props = {
     price: string;
     img: string;
     link: string;
+    onClick: () => void;
 }
 
-export default function BottomSheetHome({ name, description, price, img, link }: Props) {
+export default function BottomSheetHome({ name, description, price, img, link, onClick }: Props) {
     const navigate = useNavigate()
     return (
-        <S.BottomSheetHomeContainer>
-            <S.SheetHeader>
-                <S.Img src={img} alt="" />
-                <S.ContentTitle>
-                    <S.Title>{name}</S.Title>
-                    <S.Description>{description}</S.Description>
-                    <S.Price>{price}</S.Price>
-                </S.ContentTitle>
-            </S.SheetHeader>
-            <S.AverageDiv>
-                <S.ImgStar src={star} alt="" />
-                <S.AverageContent>
-                    <S.AverageTitle>Average Rating</S.AverageTitle>
-                    <S.AverageParagraph>Average Rating</S.AverageParagraph>
-                </S.AverageContent>
-                <S.Arrow src={arrow} alt="" />
-            </S.AverageDiv>
-            <S.SelectSize>
-                <S.SizeContent>
-                    <S.SelectSizeTitle>Select Size</S.SelectSizeTitle>
-                    <S.SelectSizeParagraph>(UK Size)</S.SelectSizeParagraph>
-                </S.SizeContent>
-                <S.SizeTabs>
-                    <S.Tab>7</S.Tab>
-                    <S.Tab>8</S.Tab>
-                    <S.Tab>9</S.Tab>
-                    <S.Tab>10</S.Tab>
-                </S.SizeTabs>
-            </S.SelectSize>
-            <S.Buttons>
-                <S.ImgPlaceHolderDiv onClick={() => navigate(`/${link}`)}>
-                    <img src={placeholder} alt="" />
-                </S.ImgPlaceHolderDiv>
-                <ButtonAdd />
-            </S.Buttons>
-        </S.BottomSheetHomeContainer>
+        <>
+            <S.BottomSheetHomeDark onClick={onClick}></S.BottomSheetHomeDark>
+            <S.BottomSheetHomeContainer>
+                <S.SheetHeader>
+                    <S.Img src={img} alt="" />
+                    <S.ContentTitle>
+                        <S.Title>{name}</S.Title>
+                        <S.Description>{description}</S.Description>
+                        <S.Price>{price}</S.Price>
+                    </S.ContentTitle>
+                </S.SheetHeader>
+                <S.AverageDiv>
+                    <S.ImgStar src={star} alt="" />
+                    <S.AverageContent>
+                        <S.AverageTitle>Average Rating</S.AverageTitle>
+                        <S.AverageParagraph>Average Rating</S.AverageParagraph>
+                    </S.AverageContent>
+                    <S.Arrow src={arrow} alt="" />
+                </S.AverageDiv>
+                <S.SelectSize>
+                    <S.SizeContent>
+                        <S.SelectSizeTitle>Select Size</S.SelectSizeTitle>
+                        <S.SelectSizeParagraph>(UK Size)</S.SelectSizeParagraph>
+                    </S.SizeContent>
+                    <S.SizeTabs>
+                        <S.Tab>7</S.Tab>
+                        <S.Tab>8</S.Tab>
+                        <S.Tab>9</S.Tab>
+                        <S.Tab>10</S.Tab>
+                    </S.SizeTabs>
+                </S.SelectSize>
+                <S.Buttons>
+                    <S.ImgPlaceHolderDiv onClick={() => navigate(link)}>
+                        <img src={placeholder} alt="" />
+                    </S.ImgPlaceHolderDiv>
+                    <ButtonAdd name={name} description={description} price={price} img={img} />
+                </S.Buttons>
+            </S.BottomSheetHomeContainer>
+        </>
     )
 }
