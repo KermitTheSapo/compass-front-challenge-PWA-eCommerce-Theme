8ead5ef9c8424303ccba3dc9df414eacce87d81f
Robert <rjsf06@gmail.com>
2022-11-23 18:09:26 -0300

    :lipstick: feat: doing the bottomnavigation style

--- src/stories/bottomNavigation/bottomNavigationStyle.ts
+++ src/stories/bottomNavigation/bottomNavigationStyle.ts
@@ -1,5 +1,28 @@
 import styled from "styled-components";
 
 export const BottomNavigationContainer = styled.div`
-    
+    width: 360px;
+    height: 56px;
+    background-color: var(--Primary);
+    display: flex;
+    align-items: center;
+`
+
+export const Tab = styled.div`
+    height: 50%;
+    width: 25%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: column;
+    gap: 4px;
+    cursor: pointer;
+`
+
+export const ImgIcon = styled.img``
+
+export const labelParagraph = styled.p`
+    font-size: 0.75rem;
+    font-weight: 400;
+    color: var(--Bright);
 `
