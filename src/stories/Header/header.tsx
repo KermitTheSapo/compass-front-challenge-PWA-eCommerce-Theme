649442ecbe94239742f51395a84c943315c05da1
Robert <rjsf06@gmail.com>
2022-11-23 18:16:27 -0300

    :sparkles: feat: doing some adjustments in the header stories

--- src/stories/Header/header.tsx
+++ src/stories/Header/header.tsx
@@ -8,61 +8,59 @@ import profile from "../../assets/imgs/header/profile.svg"
 import bag from "../../assets/imgs/header/bag.svg"
 import * as S from "./headerStyle"
 import { Reset } from "../../assets/styles/reset"
-import Header from "src/components/Header/Header"
 
 
 export const HeaderS = () => (
-    <Header />
-    // <S.HeaderContainer>
-    //     <Reset />
-    //     <S.MenuContainer>
-    //         <S.MenuImg src={menu} alt="" />
-    //         <S.TitlePage>Home</S.TitlePage>
-    //     </S.MenuContainer>
-    //     <S.NavCategory>
-    //         <S.LinkCategory href="/error">
-    //             <S.ImgLogo src={imgLogo} alt="" />
-    //         </S.LinkCategory>
-    //         <S.UlCategory>
-    //             <S.LinkCategory href="/error">
-    //                 <S.LiCategory>Handbags</S.LiCategory>
-    //             </S.LinkCategory>
-    //             <S.LinkCategory href="/error">
-    //                 <S.LiCategory>Watches</S.LiCategory>
-    //             </S.LinkCategory>
-    //             <S.LinkCategory href="/error">
-    //                 <S.LiCategory>Skincare</S.LiCategory>
-    //             </S.LinkCategory>
-    //             <S.LinkCategory href="/error">
-    //                 <S.LiCategory>Jewellery</S.LiCategory>
-    //             </S.LinkCategory>
-    //             <S.LinkCategory href="/error">
-    //                 <S.LiCategory>Apparels</S.LiCategory>
-    //             </S.LinkCategory>
-    //         </S.UlCategory>
-    //     </S.NavCategory>
-    //     <S.IconsContainer>
-    //         <S.Input type="text" placeholder="Search for products or brands..." />
-    //         <S.LinkCategory href="/error">
-    //             <S.IconImg src={heart} alt="" />
-    //         </S.LinkCategory>
-    //         <S.LinkCategory href="/error">
-    //             <S.IconImg src={profile} alt="" />
-    //         </S.LinkCategory>
-    //         <S.LinkCategory href="/error">
-    //             <S.IconImg src={bag} alt="" />
-    //         </S.LinkCategory>
-    //     </S.IconsContainer>
-    //     <S.TrailingIconsContainer>
-    //         <S.LinkCategory href="/error">
-    //             <S.IconImgMobile src={add} alt="" />
-    //         </S.LinkCategory>
-    //         <S.LinkCategory href="/error">
-    //             <S.IconImgMobile src={notification} alt="" />
-    //         </S.LinkCategory>
-    //         <S.LinkCategory href="/error">
-    //             <S.IconImgMobile src={search} alt="" />
-    //         </S.LinkCategory>
-    //     </S.TrailingIconsContainer>
-    // </S.HeaderContainer>
+    <S.HeaderContainer>
+        <Reset />
+        <S.MenuContainer>
+            <S.MenuImg src={menu} alt="" />
+            <S.TitlePage>Home</S.TitlePage>
+        </S.MenuContainer>
+        <S.NavCategory>
+            <S.LinkCategory href="/error">
+                <S.ImgLogo src={imgLogo} alt="" />
+            </S.LinkCategory>
+            <S.UlCategory>
+                <S.LinkCategory href="/error">
+                    <S.LiCategory>Handbags</S.LiCategory>
+                </S.LinkCategory>
+                <S.LinkCategory href="/error">
+                    <S.LiCategory>Watches</S.LiCategory>
+                </S.LinkCategory>
+                <S.LinkCategory href="/error">
+                    <S.LiCategory>Skincare</S.LiCategory>
+                </S.LinkCategory>
+                <S.LinkCategory href="/error">
+                    <S.LiCategory>Jewellery</S.LiCategory>
+                </S.LinkCategory>
+                <S.LinkCategory href="/error">
+                    <S.LiCategory>Apparels</S.LiCategory>
+                </S.LinkCategory>
+            </S.UlCategory>
+        </S.NavCategory>
+        <S.IconsContainer>
+            <S.Input type="text" placeholder="Search for products or brands..." />
+            <S.LinkCategory href="/error">
+                <S.IconImg src={heart} alt="" />
+            </S.LinkCategory>
+            <S.LinkCategory href="/error">
+                <S.IconImg src={profile} alt="" />
+            </S.LinkCategory>
+            <S.LinkCategory href="/error">
+                <S.IconImg src={bag} alt="" />
+            </S.LinkCategory>
+        </S.IconsContainer>
+        <S.TrailingIconsContainer>
+            <S.LinkCategory href="/error">
+                <S.IconImgMobile src={add} alt="" />
+            </S.LinkCategory>
+            <S.LinkCategory href="/error">
+                <S.IconImgMobile src={notification} alt="" />
+            </S.LinkCategory>
+            <S.LinkCategory href="/error">
+                <S.IconImgMobile src={search} alt="" />
+            </S.LinkCategory>
+        </S.TrailingIconsContainer>
+    </S.HeaderContainer>
 );
