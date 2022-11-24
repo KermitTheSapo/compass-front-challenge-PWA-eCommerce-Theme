32d76c181f136afe7f9d602a7f5a021aff6c6f91
Robert <rjsf06@gmail.com>
2022-11-24 12:57:03 -0300

    :lipstick: feat: adding a cursor pointer and props in the title

--- src/stories/sideNavigationMenu/sideNavigationMenuStyle.ts
+++ src/stories/sideNavigationMenu/sideNavigationMenuStyle.ts
@@ -5,4 +5,18 @@ export const SideNavigationMenuContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    width: 328px;
+    border-bottom: 1px solid #0000001F;
+    height: 6vh;
+    cursor: pointer;
+`
+
+export const Title = styled.h3`
+    font-weight: 500;
+    font-size: 1rem;
+    color: ${p => p.color};
+`
+
+export const Img = styled.img`
+
 `
