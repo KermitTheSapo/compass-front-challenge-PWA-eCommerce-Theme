218bf8570c0129279bb925e9ed261d4f85f8ee89
Robert <rjsf06@gmail.com>
2022-11-25 15:04:20 -0300

    :sparkles: feat: adding the counter state to be used with props in the QuantityCounter component props

--- src/components/productPage/quantity/quantity.tsx
+++ src/components/productPage/quantity/quantity.tsx
@@ -1,14 +1,15 @@
+import { useState } from "react"
 import QuantityCounter from "./quantityCounter/quantityCounter"
 import * as S from "./quantityStyle"
 
 
 export default function Quantity() {
-
+    const [counter, setCounter] = useState(1)
 
     return (
         <S.Quantity>
             <S.QuantityTitle>Quantity:</S.QuantityTitle>
-            <QuantityCounter  />
+            <QuantityCounter setState={setCounter} state={counter} />
         </S.Quantity>
     )
 }
