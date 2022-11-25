2fc4925989efd3497cd0fa4d06cdcce8d90b8f35
Robert <rjsf06@gmail.com>
2022-11-24 21:49:56 -0300

    :sparkles: feat: adding the ratings, the link2 and information in the itemArrival and applying the key in the map

--- src/components/productPage/alsoLike/alsoLikeItem/alsoLikeItem.tsx
+++ src/components/productPage/alsoLike/alsoLikeItem/alsoLikeItem.tsx
@@ -5,9 +5,9 @@ import ProductsItem from "../../../../products/products.json"
 export default function AlsoLikeItem() {
     return (
         <S.AlsoLikeProducts>
-            {ProductsItem.map((item) => (
-                <S.Div>
-                    <ItemArrival ImgSrc={item.img} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item.link} />
+            {ProductsItem.map((item, key) => (
+                <S.Div key={key}>
+                    <ItemArrival ImgSrc={item.img} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item.link} ratings={item.ratings} link2={""} information={false} />
                     <S.BtnTrending>
                         <S.SpanTrending>Trending</S.SpanTrending>
                     </S.BtnTrending>
