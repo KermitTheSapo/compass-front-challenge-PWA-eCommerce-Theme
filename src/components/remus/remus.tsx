b4ec3170b85d089caa3d695b568d38e7a967c140
Robert <rjsf06@gmail.com>
2022-11-24 21:44:45 -0300

    :sparkles: feat: adding the ratings in the remus productPage

--- src/components/remus/remus.tsx
+++ src/components/remus/remus.tsx
@@ -4,7 +4,7 @@ import products from "../..//products/products.json"
 export default function Remus() {
     return (
         <>
-            <ProductPage name={products[2].name} img={products[2].img} discount={products[2].discount} productDescription="Experience comfortable and easy travelling like never before with this coach bag. It features a zip closure, removable straps and multiple organization compartments to keep your valuables safe. Crafted from premium material, it is durable and lasts long." productParagraph={products[2].description} safe={products[2].safe} value={products[2].price} imgCarousel={products[2].img} />
+            <ProductPage name={products[2].name} img={products[2].img} discount={products[2].discount} productDescription="Experience comfortable and easy traveling like never before with this coach bag. It features a zip closure, removable straps and multiple organization compartments to keep your valuables safe. Crafted from premium material, it is durable and lasts long." productParagraph={products[2].description} safe={products[2].safe} value={products[2].price} imgCarousel={products[2].img} ratings={products[2].ratings} />
             <Helmet>
                 <title>Coral'l | Remus</title>
             </Helmet>
