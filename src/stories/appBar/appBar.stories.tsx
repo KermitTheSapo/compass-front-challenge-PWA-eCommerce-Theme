175395fe8cf80196d8ed04ebae7e51bb79feb8ab
Robert <rjsf06@gmail.com>
2022-11-24 21:39:09 -0300

    :sparkles: feat: doing the pageTitle, pageInfo and search ArgTypes, and using the props to story can be editable

--- src/stories/appBar/appBar.stories.tsx
+++ src/stories/appBar/appBar.stories.tsx
@@ -3,10 +3,20 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { AppBar } from "./appBar";
 
 export default {
-  title: 'HeaderAndFooters/AppBar',
-  component: AppBar
+  title: 'Header And Footers/AppBar',
+  component: AppBar,
+  argTypes: {
+    pageTitle: { control: 'text' },
+    pageInfo: { control: 'text' },
+    Search: { control: 'text' },
+  },
 } as ComponentMeta<typeof AppBar>;
 
-const Template: ComponentStory<typeof AppBar> = () => <AppBar />;
+const Template: ComponentStory<typeof AppBar> = (args) => <AppBar  {...args} />;
 
 export const appBarExample = Template.bind({});
+appBarExample.args = {
+  pageTitle: 'Page Title',
+  pageInfo: "page Info",
+  Search: "Search",
+};
--- src/stories/appBar/appBar.tsx
+++ src/stories/appBar/appBar.tsx
@@ -2,35 +2,41 @@ import * as S from "./appBarStyle"
 import { Reset } from "../../assets/styles/reset"
 import icon from "../assets/appBar/icon-placeholder.svg"
 
-export const AppBar = () => (
-        <S.AppBarContainer>
-            <Reset />
-            <S.Div>
-                <S.TitleDiv>
-                    <S.LeadingIcon>
-                        <S.LeadingIconImg src={icon} alt="" />
-                        <S.Content>
-                            <S.Text>Page Title</S.Text>
-                            <S.Paragraph>Page Info</S.Paragraph>
-                        </S.Content>
-                    </S.LeadingIcon>
-                    <S.TrailingIcon>
-                        <S.IconPlaceholder src={icon} alt="" />
-                        <S.IconPlaceholder src={icon} alt="" />
-                        <S.IconPlaceholder src={icon} alt="" />
-                    </S.TrailingIcon>
-                </S.TitleDiv>
-                <S.PageInfoDiv>
-                    <S.LeadingIcon>
-                        <S.LeadingIconImg src={icon} alt="" />
-                        <S.Text>Search</S.Text>
-                    </S.LeadingIcon>
-                    <S.TrailingIcon>
-                        <S.IconPlaceholder src={icon} alt="" />
-                        <S.IconPlaceholder src={icon} alt="" />
-                        <S.IconPlaceholder src={icon} alt="" />
-                    </S.TrailingIcon>
-                </S.PageInfoDiv>
-            </S.Div>
-        </S.AppBarContainer>
+interface AppBarProps {
+    pageTitle: string;
+    pageInfo: string;
+    Search: string;
+}
+
+export const AppBar = ({ pageTitle, pageInfo, Search }: AppBarProps) => (
+    <S.AppBarContainer>
+        <Reset />
+        <S.Div>
+            <S.TitleDiv>
+                <S.LeadingIcon>
+                    <S.LeadingIconImg src={icon} alt="" />
+                    <S.Content>
+                        <S.Text>{pageTitle}</S.Text>
+                        <S.Paragraph>{pageInfo}</S.Paragraph>
+                    </S.Content>
+                </S.LeadingIcon>
+                <S.TrailingIcon>
+                    <S.IconPlaceholder src={icon} alt="" />
+                    <S.IconPlaceholder src={icon} alt="" />
+                    <S.IconPlaceholder src={icon} alt="" />
+                </S.TrailingIcon>
+            </S.TitleDiv>
+            <S.PageInfoDiv>
+                <S.LeadingIcon>
+                    <S.LeadingIconImg src={icon} alt="" />
+                    <S.Text>{Search}</S.Text>
+                </S.LeadingIcon>
+                <S.TrailingIcon>
+                    <S.IconPlaceholder src={icon} alt="" />
+                    <S.IconPlaceholder src={icon} alt="" />
+                    <S.IconPlaceholder src={icon} alt="" />
+                </S.TrailingIcon>
+            </S.PageInfoDiv>
+        </S.Div>
+    </S.AppBarContainer>
 );
