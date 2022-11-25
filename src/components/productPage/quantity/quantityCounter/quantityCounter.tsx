c1e2c263d9f05da8f30d2a98075e76fb0ed55991
Robert <rjsf06@gmail.com>
2022-11-25 11:29:52 -0300

    :sparkles: feat: changing the values for props

--- src/components/productPage/quantity/quantityCounter/quantityCounter.tsx
+++ src/components/productPage/quantity/quantityCounter/quantityCounter.tsx
@@ -1,16 +1,25 @@
 import * as S from "./quantityCounterStyle"
 import minus from "@/assets/imgs/productPage/minus.svg"
 import plus from "@/assets/imgs/productPage/plus.svg"
-import { useState } from "react"
 
-export default function QuantityCounter() {
-    const [quantity, setQuantity] = useState(1)
+type Props = {
+    setState: React.Dispatch<React.SetStateAction<number>>;
+    state: number;
+}
+
+export default function QuantityCounter({ setState, state }: Props) {
+    const addQuantity = () => {
+        state > 1 ? setState(state - 1) : setState(state)
+    }
+    const removeQuantity = () => {
+        state < 10 ? setState(state + 1) : setState(state)
+    }
 
     return (
         <S.QuantityCounter>
-            <S.Img onClick={() => quantity > 1 ? setQuantity(quantity - 1) : setQuantity(quantity)} src={minus} alt="a plus icon" />
-            <S.QuantityNumber>{quantity}</S.QuantityNumber>
-            <S.Img onClick={() => quantity < 10 ? setQuantity(quantity + 1) : setQuantity(quantity)} src={plus} alt="a minus icon" />
+            <S.Img onClick={() => { addQuantity() }} src={minus} alt="a plus icon" />
+            <S.QuantityNumber>{state}</S.QuantityNumber>
+            <S.Img onClick={() => { removeQuantity() }} src={plus} alt="a minus icon" />
         </S.QuantityCounter>
     )
 }
