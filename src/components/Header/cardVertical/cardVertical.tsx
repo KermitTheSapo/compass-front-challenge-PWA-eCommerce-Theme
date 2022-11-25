38438053df4dd5424a6d0f44919262c8e2426f3f
Robert <rjsf06@gmail.com>
2022-11-25 11:33:23 -0300

    :sparkles: feat: making product value increase based on counter

--- src/components/Header/cardVertical/cardVertical.tsx
+++ src/components/Header/cardVertical/cardVertical.tsx
@@ -1,6 +1,7 @@
 import QuantityCounter from "../../productPage/quantity/quantityCounter/quantityCounter"
 import * as S from "./cardVerticalStyle"
 import deleteImg from "@/assets/imgs/header/deleteImg.svg"
+import { useEffect, useState } from "react";
 
 type Props = {
     productTitle: string;
@@ -10,7 +11,14 @@ type Props = {
 }
 
 export default function CardVertical({ productTitle, productParagraph, productPrice, img }: Props) {
-
+    const [quantityMultiply, setQuantityMultiply] = useState(1)
+    const [valueMultiply, setValueMultiply] = useState(productPrice * quantityMultiply)
+    const multiply = () => {
+        setValueMultiply(productPrice * quantityMultiply)
+    }
+    useEffect(() => {
+        multiply()
+    }, [quantityMultiply])
     return (
         <S.CardVertical>
             <S.CardVerticalImgDiv>
@@ -19,11 +27,11 @@ export default function CardVertical({ productTitle, productParagraph, productPr
             <S.ProductDescription>
                 <S.ProductTitle>{productTitle}</S.ProductTitle>
                 <S.ProductParagraph>{productParagraph}</S.ProductParagraph>
-                <QuantityCounter />
+                <QuantityCounter setState={setQuantityMultiply} state={quantityMultiply} />
             </S.ProductDescription>
             <S.PriceDeleteDiv>
                 <S.BtnDelete src={deleteImg} alt="an X icon" />
-                <S.PriceText>{productPrice}</S.PriceText>
+                <S.PriceText>{valueMultiply.toFixed(2)}</S.PriceText>
             </S.PriceDeleteDiv>
         </S.CardVertical>
     )
