d69a634ebb498d355ac161f427810aad1a49eba4
Robert <rjsf06@gmail.com>
2022-11-24 21:48:39 -0300

    :sparkles: feat: making some spelling corrections

--- src/components/productPage/inviteFriends/inviteFriends.tsx
+++ src/components/productPage/inviteFriends/inviteFriends.tsx
@@ -7,7 +7,7 @@ export default function InviteFriends() {
         <S.InviteFriends>
             <S.InviteFriendsContent>
                 <S.InviteFriendsTitle>Invite Friends & Earn</S.InviteFriendsTitle>
-                <S.InviteFriendsParagraph>Invite yourGet uptp 100 reward points for every friend you invite</S.InviteFriendsParagraph>
+                <S.InviteFriendsParagraph>Invite yourGet up to 100 reward points for every friend you invite</S.InviteFriendsParagraph>
                 <S.LinkInvite>
                     <S.InviteText>Invite Now</S.InviteText>
                     <S.ImgArrowInvite src={inviteArrow} alt="blue right arrow" />
