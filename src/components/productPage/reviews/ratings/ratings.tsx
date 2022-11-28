a647d5a8d363df332968d094c214c019c9baaade
Robert <rjsf06@gmail.com>
2022-11-28 20:56:38 -0300

    :sparkles: feat: adding the ratings structure and style

--- src/components/productPage/reviews/ratings/ratings.tsx
+++ src/components/productPage/reviews/ratings/ratings.tsx
@@ -1,9 +1,31 @@
 import * as S from "./ratingsStyle"
-
+import star from "@/assets/imgs/productPage/reviews/star.svg"
+import img from "@/assets/imgs/productPage/reviews/image.png"
 export default function Ratings() {
     return (
         <S.RatingsContainer>
-            <h1>asdf</h1>
+            <S.HeaderInformation>
+                <S.RatingsPill>
+                    <S.RatingNumber>4.0</S.RatingNumber>
+                    <S.StarImg src={star} alt="" />
+                </S.RatingsPill>
+                <S.RatingsPersonInfo>
+                    <S.RatingsPersonName>John Doe</S.RatingsPersonName>
+                    <S.RatingsPersonDate>12/12/2020</S.RatingsPersonDate>
+                </S.RatingsPersonInfo>
+            </S.HeaderInformation>
+            <S.Content>
+                <S.RatingsTitle>Must go for the class feel.</S.RatingsTitle>
+                <S.RatingsDescription>Totally amazing! I loved the material and the quality. It has a jolly vibe in it which makes me feel happy every time I put it on.</S.RatingsDescription>
+            </S.Content>
+            <S.ImgsCarousel>
+                <S.ImgCarousel src={img} alt="" />
+                <S.ImgCarousel src={img} alt="" />
+                <S.ImgCarousel src={img} alt="" />
+                <S.ImgCarousel src={img} alt="" />
+                <S.ImgCarousel src={img} alt="" />
+                <S.ImgCarousel src={img} alt="" />
+            </S.ImgsCarousel>
         </S.RatingsContainer>
     )
 }
--- src/components/productPage/reviews/ratings/ratingsStyle.ts
+++ src/components/productPage/reviews/ratings/ratingsStyle.ts
@@ -1,3 +1,80 @@
 import styled from "styled-components";
 
-export const RatingsContainer = styled.div``
+export const RatingsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const HeaderInformation = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 45%;
+    padding: 32px 0px 12px 16px;
+`
+export const RatingsPill = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 4px;
+    background-color: var(--Accent);
+    height: 38px;
+    width: 74px;
+`
+export const RatingNumber = styled.p`
+    
+`
+export const StarImg = styled.img`
+
+`
+
+export const RatingsPersonInfo = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+`
+export const RatingsPersonName = styled.h3`
+    font-weight: 600;
+    font-size: 0.875rem;
+    color: var(--TypeHighEmphasis);
+    `
+export const RatingsPersonDate = styled.p`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--TypeLowEmphasis);
+`
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    padding: 0 16px;
+    height: 10vh;
+`
+export const RatingsTitle = styled.div`
+    font-weight: 600;
+    font-size: 0.875rem;
+    color: var(--TypeHighEmphasis);
+`
+export const RatingsDescription = styled.div`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--TypeLowEmphasis);
+`
+export const ImgsCarousel = styled.div`
+    display: flex;
+    display: flex;
+    min-width: 100%;
+    overflow: auto;
+    gap: 15px;
+    padding: 0 0 0 16px;
+    &::-webkit-scrollbar { 
+  display: none;
+}
+`
+export const ImgCarousel = styled.img`
+    height: 80px;
+    width: 80px;
+`
+
+
+// export const ImgCarousel = styled.div``
