cf76b22c8610d567b2b7500a42d6b3ab9b8dcc17
Robert <rjsf06@gmail.com>
2022-11-28 20:57:29 -0300

    :sparkles: feat: adding the reviews structure and style

--- src/components/productPage/reviews/reviews.tsx
+++ src/components/productPage/reviews/reviews.tsx
@@ -1,9 +1,30 @@
 import * as S from "./reviewsStyle"
 
+import arrowLeft from "@/assets/imgs/productPage/reviews/arrowLeft.svg"
+import { useNavigate } from "react-router-dom";
+import Head from "./head/head";
+import Ratings from "./ratings/ratings";
+
+
+
 export default function Reviews() {
+    const navigate = useNavigate();
+
     return (
         <S.ReviewsContainer>
-            <h1>asdf</h1>
+            <S.HeaderNav>
+                <S.ImgNavArrow onClick={() => navigate(-1)} src={arrowLeft} alt="" />
+            </S.HeaderNav>
+            <Head />
+            <S.Separator></S.Separator>
+            <Ratings />
+            <Ratings />
+            <Ratings />
+            <Ratings />
+            <Ratings />
+            <S.ButtonAdd>
+                <S.ButtonAddText onClick={() => navigate("/addReview")}>Write a Review</S.ButtonAddText>
+            </S.ButtonAdd>
         </S.ReviewsContainer>
     )
 }
--- src/components/productPage/reviews/reviewsStyle.ts
+++ src/components/productPage/reviews/reviewsStyle.ts
@@ -1,5 +1,53 @@
 import styled from "styled-components";
+import plus from "@/assets/imgs/productPage/reviews/plus.svg"
 
 export const ReviewsContainer = styled.div`
 
+`
+
+export const HeaderNav = styled.header`
+    display: flex;
+    padding: 0 16px;
+    align-items: center;
+    height: 8vh;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background-color: var(--Bright);
+`
+export const ImgNavArrow = styled.img`
+
+`
+
+export const Separator = styled.div`
+    height: 8px;
+    width: 100%;
+    background-color: var(--Grey);
+`
+
+export const ButtonAdd = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--Bright);
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 8vh;
+`
+
+export const ButtonAddText = styled.button`
+    width: 90%;
+    height: 80%;
+    border-radius: 8px;
+    background-color: var(--Primary);
+    color: var(--Bright);
+    font-weight: 600;
+    font-size: 0.875rem;
+    border: none;
+    outline: none;
+    background-image: url(${plus});
+    background-repeat: no-repeat;
+    background-position:  110px center;
+    background-size: 25px;
 `
