5a307f0222180f49ff844c49e8cfc04def3cd0f3
Robert <rjsf06@gmail.com>
2022-11-29 15:49:35 -0300

    :sparkles: feat: doing the paymentMethod structure and style

---
+++ src/components/payments/paymentMethod/paymentMethod.tsx
@@ -0,0 +1,62 @@
+import * as S from "./paymentMethodStyle"
+import plus from "@/assets/imgs/payments/plus.svg"
+import googlePlay from "@/assets/imgs/payments/googlePlay.svg"
+import phonePe from "@/assets/imgs/payments/phonePe.svg"
+import paytm from "@/assets/imgs/payments/paytm.svg"
+import minus from "@/assets/imgs/payments/minus.svg"
+import { useState } from "react"
+export default function PaymentMethod() {
+    const [showUpi, setShowUpi] = useState(false)
+    return (
+        <S.PaymentMethodContainer>
+            <S.PaymentOption>
+                <S.PaymentHeader>
+                    <S.PaymentMethodTitle>Debit Card/Credit Card</S.PaymentMethodTitle>
+                    <S.ImgModal src={plus} alt="" />
+                </S.PaymentHeader>
+            </S.PaymentOption>
+            <S.PaymentOption>
+                <S.PaymentHeader>
+                    <S.PaymentMethodTitle>UPI</S.PaymentMethodTitle>
+                    <S.ImgModal onClick={() => setShowUpi(!showUpi)} src={showUpi ? minus : plus} alt="" />
+                </S.PaymentHeader>
+                {showUpi &&
+                    <>
+                        <S.MethodList>
+                            <S.MethodItem>
+                                <S.Label htmlFor="">
+                                    <S.ImgPayment src={googlePlay} alt="" />
+                                    <S.NamePayment>Google Pay</S.NamePayment>
+                                </S.Label>
+                                <S.InputRadio type="radio" name="upi" />
+                            </S.MethodItem>
+                            <S.MethodItem>
+                                <S.Label htmlFor="">
+                                    <S.ImgPayment src={phonePe} alt="" />
+                                    <S.NamePayment>Phone Pe</S.NamePayment>
+                                </S.Label>
+                                <S.InputRadio type="radio" name="upi" />
+                            </S.MethodItem>
+                            <S.MethodItem>
+                                <S.Label htmlFor="">
+                                    <S.ImgPayment src={paytm} alt="" />
+                                    <S.NamePayment>Paytm</S.NamePayment>
+                                </S.Label>
+                                <S.InputRadio type="radio" name="upi" />
+                            </S.MethodItem>
+                        </S.MethodList>
+                        <S.UpiID>
+                            <S.InputDiv>
+                                <S.InputUpi type="text" placeholder="Enter your UPI Id" />
+                                <S.UpiExample>Eg: 1234567890@ybl</S.UpiExample>
+                            </S.InputDiv>
+                            <S.SaveBtn>
+                                <S.InputCheckbox type="checkbox" name="" id="" />
+                                <S.LabelCheckbox htmlFor="">Save this for future transactions</S.LabelCheckbox>
+                            </S.SaveBtn>
+                        </S.UpiID>
+                    </>}
+            </S.PaymentOption>
+        </S.PaymentMethodContainer>
+    )
+}
---
+++ src/components/payments/paymentMethod/paymentMethodStyle.ts
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+
+export const PaymentMethodContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const PaymentOption = styled.div`
+    display: flex;
+    flex-direction: column;
+
+`
+export const PaymentMethodTitle = styled.h3`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--TypeHighEmphasis);
+`
+export const ImgModal = styled.img`
+    height: 24px;
+    width: 24px;
+`
+
+export const PaymentHeader = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 56px;
+`
+export const MethodList = styled.ul`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    gap: 16px;
+    height: 20vh;
+`
+export const MethodItem = styled.li`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+export const Label = styled.label`
+    display: flex;
+    align-items: center;
+`
+export const ImgPayment = styled.img`
+    margin-right: 12px;
+`
+export const NamePayment = styled.p`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--TypeHighEmphasis);
+`
+export const InputRadio = styled.input``
+
+export const UpiID = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 8px;
+    border: solid 1px var(--Grey);
+    padding: 0 16px;
+    height: 142px;
+    gap: 24px;
+`
+
+export const InputDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: 10vh;
+`
+export const SaveBtn = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+export const InputUpi = styled.input`
+    border: none;
+    height: 52px;
+    border-radius: 4px;
+    background-color: var(--Grey);
+    font-weight: 500;
+    font-size: 1rem;
+    color: var(--TypeLowEmphasis);
+    padding: 0 16px;
+`
+export const UpiExample = styled.p`
+    font-weight: 500;
+    font-size: 0.75rem;
+    color: var(--LightText);
+`
+export const InputCheckbox = styled.input`
+
+`
+export const LabelCheckbox = styled.label`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--TypeLowEmphasis);
+`
