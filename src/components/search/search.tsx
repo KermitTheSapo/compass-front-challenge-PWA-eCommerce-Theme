92acb31c4679a1c10550a0bad21c93a6610f2d23
Robert <rjsf06@gmail.com>
2022-11-30 19:58:22 -0300

    :sparkles: faet: using the mongodb api informations in the map

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -2,12 +2,29 @@ import * as S from "./searchStyle"
 
 import arrowBack from "@/assets/imgs/search/arrowLeft.svg"
 import { useNavigate } from "react-router-dom"
-import ProductsItem from "../../products/products.json"
 import ItemArrival from "../../components/Home/main/Arrivals/itemArrival/itemArrival"
 import { Helmet } from "react-helmet"
+import { useEffect, useState } from "react"
+import { getProducts } from "../../products/products";
 
 export default function Search() {
     const navigate = useNavigate()
+    const [input, setInput] = useState("")
+    const [productsList, setProductsList] = useState([{
+        _id: "",
+        name: "",
+        price: 0,
+        category: "",
+        image: "",
+        description: "",
+        imgAlt: "",
+        paragraph: "",
+        link: "",
+        ratings: 0
+    }])
+    useEffect(() => {
+        getProducts().then((res) => setProductsList(res))
+    }, [])
     return (
         <S.SearchContainer>
             <Helmet>
@@ -15,7 +32,7 @@ export default function Search() {
             </Helmet>
             <S.SearchHeader>
                 <S.ArrowBack onClick={() => { navigate(-1) }} src={arrowBack} alt="" />
-                <S.InputSearch type="text" placeholder="Search" />
+                <S.InputSearch type="text" placeholder="Search" value={input} onChange={(e) => setInput(e.target.value)} />
             </S.SearchHeader>
             <S.RecentSearch>
                 <S.RecentSearchTitle>Recent Search</S.RecentSearchTitle>
@@ -30,10 +47,8 @@ export default function Search() {
             <S.NewArrivals>
                 <S.NewArrivalsTitle>New Arrivals</S.NewArrivalsTitle>
                 <S.NewArrivalsList>
-                    {ProductsItem.map((item, key) => (
-                        <S.Div key={key}>
-                            <ItemArrival ImgSrc={item.img} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item.link} ratings={item.ratings} link2={""} information={false} />
-                        </S.Div>
+                    {productsList && productsList.map((item, key) => (
+                        <ItemArrival ImgSrc={item.image} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item._id} ratings={item.ratings} link2={""} information={false} />
                     ))}
                 </S.NewArrivalsList>
             </S.NewArrivals>
