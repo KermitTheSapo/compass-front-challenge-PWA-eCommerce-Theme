e360dcde6bc10207bcb67f3d13f0a3601004bc89
Robert <rjsf06@gmail.com>
2022-11-30 19:45:30 -0300

    :sparkles: feat: consuming the mongodb API and using in the map

--- src/components/productPage/alsoLike/alsoLikeItem/alsoLikeItem.tsx
+++ src/components/productPage/alsoLike/alsoLikeItem/alsoLikeItem.tsx
@@ -1,17 +1,33 @@
 import * as S from "./alsoLikeItemStyle"
 import ItemArrival from "../../../Home/main/Arrivals/itemArrival/itemArrival"
-import ProductsItem from "../../../../products/products.json"
-
+import { getProducts } from "../../../../products/products";
+import { useEffect, useState } from "react";
 type Props = {
     value: boolean;
 }
 
 export default function AlsoLikeItem({ value }: Props) {
+    const [productsList, setProductsList] = useState([{
+        _id: "",
+        name: "",
+        price: 0,
+        category: "",
+        image: "",
+        description: "",
+        imgAlt: "",
+        paragraph: "",
+        link: "",
+        ratings: 0
+    }])
+    useEffect(() => {
+        getProducts().then((res) => setProductsList(res))
+    }, [])
+    console.log(productsList)
     return (
         <S.AlsoLikeProducts>
-            {ProductsItem.map((item, key) => (
+            {productsList && productsList.map((item, key) => (
                 <S.Div key={key}>
-                    <ItemArrival ImgSrc={item.img} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item.link} ratings={item.ratings} link2={""} information={value} />
+                    <ItemArrival ImgSrc={item.image} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item._id} ratings={item.ratings} link2={""} information={value} />
                     <S.BtnTrending>
                         <S.SpanTrending>Trending</S.SpanTrending>
                     </S.BtnTrending>
