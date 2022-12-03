0bd19185021a1c6653aa27b73f1ce9b283c5cf6e
Robert <rjsf06@gmail.com>
2022-12-03 15:07:06 -0300

    :sparkles: feat: adding the /wishlist crud in the mongodb api with axios

---
+++ src/products/wishlist.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const Api = `http://localhost:4000/wishlist`
+export const getWishlist = async () => {
+    return axios.get(Api).then((res) => {
+        let data = res.data
+        return data;
+    });
+}
+
+export const postWishlist = async (value) => {
+    return axios.post(Api, value).then((res) => {
+        let data = res.data
+        return data;
+    });
+}
+export const deleteWishlist = async (value) => {
+    return axios.delete(`${Api}/${value}`).then((res) => {
+        let data = res.data
+        return data;
+    });
+}
