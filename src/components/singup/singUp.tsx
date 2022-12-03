affb39401fbb8b7441dd0997f7d5698498d478ab
Robert <rjsf06@gmail.com>
2022-12-02 21:15:57 -0300

    :sparkles: feat: starting the structure and style creation of the singUp page

---
+++ src/components/singup/singUp.tsx
@@ -0,0 +1,11 @@
+import * as S from "./singUpStyle"
+
+export default function SingUp() {
+    return (
+        <S.SingUpContainer>
+            <S.SingUpTitle>The shopping destination you need</S.SingUpTitle>
+            <button>Get Started</button>
+            <p>I already have an account</p>
+        </S.SingUpContainer>
+    )
+}
---
+++ src/components/singup/singUpStyle.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+import homePhoto from "@/assets/imgs/singup/homePhoto.png"
+
+export const SingUpContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
+    background-image: url(${homePhoto});
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 100vh;
+    width: 100vw;    
+`
