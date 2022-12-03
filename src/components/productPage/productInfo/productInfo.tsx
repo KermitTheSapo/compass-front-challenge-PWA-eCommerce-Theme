630bcef23fde94d6de797656cb332683b2d60fdc
Robert <rjsf06@gmail.com>
2022-12-03 15:27:22 -0300

    :sparkles: feat: with the get coupon from mongodb api, using a map in the coupon

--- src/components/productPage/productInfo/productInfo.tsx
+++ src/components/productPage/productInfo/productInfo.tsx
@@ -2,6 +2,9 @@ import * as S from "./productInfoStyle"
 import star from "@/assets/imgs/productPage/Star.svg"
 import starOn from "@/assets/imgs/productPage/star-on.svg"
 import starOff from "@/assets/imgs/productPage/star-off.svg"
+import Coupon from "./coupon/coupon"
+import { useEffect, useState } from "react"
+import { getCoupon } from "../../../products/coupon"
 
 type Props = {
     name: string;
@@ -13,6 +16,15 @@ type Props = {
 }
 
 export default function ProductInfo({ name, value, safe, discount, productParagraph, ratings }: Props) {
+    const [couponList, setCouponList] = useState([{
+        id: "",
+        name: "",
+        value: 0,
+        code: ""
+    }])
+    useEffect(() => {
+        getCoupon().then((res) => setCouponList(res))
+    })
     return (
         <S.ProductInfo>
             <S.ProductName>{name}</S.ProductName>
@@ -45,36 +57,9 @@ export default function ProductInfo({ name, value, safe, discount, productParagr
                 </S.RatingDiv>
             </S.ProductAverage>
             <S.CouponDiv>
-                <S.Coupon>
-                    <S.CouponInfo>
-                        <S.CouponTitle>Get upto 30% Off on order value above $100</S.CouponTitle>
-                        <S.CouponTerms>Terms & Conditions</S.CouponTerms>
-                    </S.CouponInfo>
-                    <S.CouponValue>
-                        <S.CouponCodeName>Use Code</S.CouponCodeName>
-                        <S.CouponCode>ORDER100</S.CouponCode>
-                    </S.CouponValue>
-                </S.Coupon>
-                <S.Coupon>
-                    <S.CouponInfo>
-                        <S.CouponTitle>Get upto 30% Off on order value above $100</S.CouponTitle>
-                        <S.CouponTerms>Terms & Conditions</S.CouponTerms>
-                    </S.CouponInfo>
-                    <S.CouponValue>
-                        <S.CouponCodeName>Use Code</S.CouponCodeName>
-                        <S.CouponCode>ORDER100</S.CouponCode>
-                    </S.CouponValue>
-                </S.Coupon>
-                <S.Coupon>
-                    <S.CouponInfo>
-                        <S.CouponTitle>Get upto 30% Off on order value above $100</S.CouponTitle>
-                        <S.CouponTerms>Terms & Conditions</S.CouponTerms>
-                    </S.CouponInfo>
-                    <S.CouponValue>
-                        <S.CouponCodeName>Use Code</S.CouponCodeName>
-                        <S.CouponCode>ORDER100</S.CouponCode>
-                    </S.CouponValue>
-                </S.Coupon>
+                {couponList.map((item) => (
+                    <Coupon name={item.name} code={item.code} />
+                ))}
             </S.CouponDiv>
         </S.ProductInfo>
     )
