c42cadb07f7feea668664a1bb91821e4a1a5bfe7
Robert <rjsf06@gmail.com>
2022-12-02 21:46:27 -0300

    :sparkles: feat: adding props in the navigation and changing the src logic

--- src/components/Home/main/bottom-navigation/navigation.tsx
+++ src/components/Home/main/bottom-navigation/navigation.tsx
@@ -8,42 +8,43 @@ import profile from "@/assets/imgs/home/navigation/profile.svg"
 import profileFill from "@/assets/imgs/home/navigation/profile-fill.svg"
 import bag from "@/assets/imgs/home/navigation/bag.svg"
 import bagFill from "@/assets/imgs/home/navigation/bag-fill.svg"
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-export default function Navigation() {
-    const [activeHome, setActiveHome] = useState(false)
-    const [activeCategories, setActiveCategories] = useState(false)
-    const [activeProfile, setActiveProfile] = useState(false)
-    const [activeBag, setActiveBag] = useState(false)
+type Props = {
+    homeTrue: boolean;
+    categoriesTrue: boolean;
+    profileTrue: boolean;
+    bagTrue: boolean;
+}
 
+export default function Navigation({ homeTrue, categoriesTrue, profileTrue, bagTrue }: Props) {
     const navigate = useNavigate();
 
 
     return (
         <S.Navigation>
-            <S.LinkNavTab onClick={() => navigate("/error")} onMouseEnter={() => setActiveHome(true)} onMouseLeave={() => setActiveHome(false)}>
+            <S.LinkNavTab onClick={() => navigate("/")}>
                 <S.NavTab>
-                    <S.Img src={activeHome ? HomeFill : Home} alt="icon of a house" />
-                    <S.Name>Home</S.Name>
+                    <S.Img src={homeTrue ? HomeFill : Home} alt="icon of a house" />
+                    <S.Name>{homeTrue ? "Home" : ""}</S.Name>
                 </S.NavTab>
             </S.LinkNavTab>
-            <S.LinkNavTab onClick={() => navigate("/error")} onMouseEnter={() => setActiveCategories(true)} onMouseLeave={() => setActiveCategories(false)}>
+            <S.LinkNavTab onClick={() => navigate("/error")}>
                 <S.NavTab>
-                    <S.Img src={activeCategories ? categoriesFill : categories} alt="4 balls icon" />
-                    <S.Name></S.Name>
+                    <S.Img src={categoriesTrue ? categoriesFill : categories} alt="4 balls icon" />
+                    <S.Name>{categoriesTrue ? "categories" : ""}</S.Name>
                 </S.NavTab>
             </S.LinkNavTab>
-            <S.LinkNavTab onClick={() => navigate("/error")} onMouseEnter={() => setActiveProfile(true)} onMouseLeave={() => setActiveProfile(false)}>
+            <S.LinkNavTab onClick={() => navigate("/profile")}>
                 <S.NavTab>
-                    <S.Img src={activeProfile ? profileFill : profile} alt="icon of the head and shoulder part of a person" />
-                    <S.Name></S.Name>
+                    <S.Img src={profileTrue ? profileFill : profile} alt="icon of the head and shoulder part of a person" />
+                    <S.Name>{profileTrue ? "profile" : ""}</S.Name>
                 </S.NavTab>
             </S.LinkNavTab>
-            <S.LinkNavTab onClick={() => navigate("/bag")} onMouseEnter={() => setActiveBag(true)} onMouseLeave={() => setActiveBag(false)}>
+            <S.LinkNavTab onClick={() => navigate("/bag")}>
                 <S.NavTab>
-                    <S.Img src={activeBag ? bagFill : bag} alt="a bag icon" />
-                    <S.Name></S.Name>
+                    <S.Img src={bagTrue ? bagFill : bag} alt="a bag icon" />
+                    <S.Name>{bagTrue ? "bag" : ""}</S.Name>
                 </S.NavTab>
             </S.LinkNavTab>
         </S.Navigation>
