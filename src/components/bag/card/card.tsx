30212edb4009ae23ef375ce0fbef8d0fb85273a4
Robert <rjsf06@gmail.com>
2022-12-02 21:39:30 -0300

    :sparkles: feat: doing the deleteProduct function with getBag and deleteBag function from the mongodb Api

--- src/components/bag/card/card.tsx
+++ src/components/bag/card/card.tsx
@@ -1,23 +1,47 @@
+import { useEffect, useState } from "react";
 import * as S from "./cardStyle"
-import img from "@/assets/imgs/bag/coach.png"
-export default function Card() {
+import { deleteBag, getBag } from "../../../products/bag";
+
+type Props = {
+    productTitle: string;
+    productParagraph: string;
+    productPrice: number;
+    img: string;
+    safe: number;
+    discount: number;
+    id: string;
+    setState: React.Dispatch<React.SetStateAction<{}[]>>;
+}
+export default function Card({ productTitle, productParagraph, productPrice, img, safe, discount, id, setState }: Props) {
+    const [quantity, setQuantity] = useState("")
+
+    const deleteProduct = () => {
+        getBag().then((res) => setState(res))
+        deleteBag(id).then((res) => { console.log(res) })
+        getBag().then((res) => setState(res))
+        alert("deleted")
+    }
     return (
         <S.CardContainer>
             <S.CardInfo>
                 <S.ImgProduct src={img} alt="" />
                 <S.Information>
-                    <S.ProductTitle>Coach</S.ProductTitle>
-                    <S.ProductParagraph>Leather Coach Bag</S.ProductParagraph>
+                    <S.ProductTitle>{productTitle}</S.ProductTitle>
+                    <S.ProductParagraph>{productParagraph}</S.ProductParagraph>
                     <S.DivQuantity>
                         <S.QntParagraph>Qty:</S.QntParagraph>
-                        <S.SelectOptions name="" id="">
-                            <option value="">1</option>
+                        <S.SelectOptions name="" id="" onChange={(e) => { setQuantity(e.target.value) }}>
+                            <option value="1">{1}</option>
+                            <option value="2">{2}</option>
+                            <option value="3">{3}</option>
+                            <option value="4">{4}</option>
+                            <option value="5">{5}</option>
                         </S.SelectOptions>
                     </S.DivQuantity>
                     <S.Values>
-                        <S.Price>$54.69</S.Price>
-                        <S.Safe>$69.99</S.Safe>
-                        <S.Discount>20% OFF</S.Discount>
+                        <S.Price>${productPrice}</S.Price>
+                        <S.Safe>${safe}</S.Safe>
+                        <S.Discount>{discount}% OFF</S.Discount>
                     </S.Values>
                 </S.Information>
             </S.CardInfo>
@@ -25,7 +49,7 @@ export default function Card() {
             <S.Actions>
                 <S.ActionText>Move to Wishlist</S.ActionText>
                 <S.VerticalSeparator></S.VerticalSeparator>
-                <S.ActionText>Remove</S.ActionText>
+                <S.ActionText onClick={() => deleteProduct()}>Remove</S.ActionText>
             </S.Actions>
         </S.CardContainer>
     )
