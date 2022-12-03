ca9ed17285b4a16ef7b909119c1e304e0106bfa9
Robert <rjsf06@gmail.com>
2022-12-03 15:30:48 -0300

    :sparkles: feat: adding more informations in the itemArrival map

--- src/components/productPage/alsoLike/alsoLikeItem/alsoLikeItem.tsx
+++ src/components/productPage/alsoLike/alsoLikeItem/alsoLikeItem.tsx
@@ -17,7 +17,9 @@ export default function AlsoLikeItem({ value }: Props) {
         imgAlt: "",
         paragraph: "",
         link: "",
-        ratings: 0
+        ratings: 0,
+        safe: 0,
+        discount: 0
     }])
     useEffect(() => {
         getProducts().then((res) => setProductsList(res))
@@ -27,7 +29,7 @@ export default function AlsoLikeItem({ value }: Props) {
         <S.AlsoLikeProducts>
             {productsList && productsList.map((item, key) => (
                 <S.Div key={key}>
-                    <ItemArrival ImgSrc={item.image} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item._id} ratings={item.ratings} link2={""} information={value} />
+                    <ItemArrival ImgSrc={item.image} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item._id} ratings={item.ratings} information={value} isButtonAddTrue={false} safe={item.safe} discount={item.discount} />
                     <S.BtnTrending>
                         <S.SpanTrending>Trending</S.SpanTrending>
                     </S.BtnTrending>
