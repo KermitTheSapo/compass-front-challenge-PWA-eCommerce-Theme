95d0a67fc0b28c1bc93ad7fb13b8e5bfe52e7dbd
Robert <rjsf06@gmail.com>
2022-12-02 21:24:42 -0300

    :sparkles: feat: making some adjustments

--- src/products/productsById.ts
+++ src/products/productsById.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export function getProductsById(id) {
-    const Api = `http://localhost:3000/product/${id}`
+    const Api = `http://localhost:4000/product/${id}`
     return axios.get(Api).then((res) => {
         let data = res.data
         return data;
