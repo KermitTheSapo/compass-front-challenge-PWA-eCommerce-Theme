2e48df12edb352b076d4c9acc4effbd329ce650f
Robert <rjsf06@gmail.com>
2022-12-02 21:36:24 -0300

    :sparkles: feat: moving files to another folder

--- src/api/config/dbConnect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-mongoose.connect(
-  'mongodb+srv://kermit:senhaCompass@pwa-ecommerce-theme-com.srdcs0o.mongodb.net/eCommerce',
-)
-
-let db = mongoose.connection
-
-export default db
