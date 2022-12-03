39a5120b840c02cb14e5e68273d44ea7a41f840a
Robert <rjsf06@gmail.com>
2022-12-03 15:15:21 -0300

    :fire: removing the productsById funcion to put in the products.ts

--- src/products/productsById.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export function getProductsById(id) {
-    const Api = `http://localhost:4000/product/${id}`
-    return axios.get(Api).then((res) => {
-        let data = res.data
-        return data;
-    });
-}
