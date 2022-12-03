5f610e8885e4c0a967b2fc4cb71aa9d22bb2b324
Robert <rjsf06@gmail.com>
2022-12-02 21:19:52 -0300

    :sparkles: feat: using the get products from mongodb api

---
+++ src/components/Header/searchBar/searchBar.tsx
@@ -0,0 +1,63 @@
+import ItemArrival from "../../../components/Home/main/Arrivals/itemArrival/itemArrival";
+import { useEffect, useState } from "react"
+import { getProducts } from "../../../products/products";
+import * as S from "./searchBarStyle"
+
+type Props = {
+    input: string
+}
+
+export default function SearchBar({ input }: Props) {
+    const [loading, setLoading] = useState(true)
+    const [arrayFiltrado, setArrayFiltrado] = useState([{
+        _id: "",
+        name: "",
+        price: 0,
+        category: "",
+        image: "",
+        description: "",
+        imgAlt: "",
+        paragraph: "",
+        link: "",
+        ratings: 0,
+        discount: 0,
+        safe: 0
+    }])
+    const [productsList, setProductsList] = useState([{
+        _id: "",
+        name: "",
+        price: 0,
+        category: "",
+        image: "",
+        description: "",
+        imgAlt: "",
+        paragraph: "",
+        link: "",
+        ratings: 0,
+        discount: 0,
+        safe: 0
+    }])
+
+    useEffect(() => {
+        getProducts().then((res) => { setProductsList(res); setLoading(false) })
+    }, [])
+
+    useEffect(() => {
+        if (productsList.length !== 0) {
+            if (input) {
+                setArrayFiltrado(productsList.filter((item) => item.name.includes(input.toLowerCase())))
+            } else {
+                setArrayFiltrado(productsList)
+            }
+        }
+    }, [input, productsList])
+
+    return (
+        <S.SearchBar>
+            {loading && <h1>Loading...</h1>}
+            {!loading ? arrayFiltrado.map((item, key) => (
+                <ItemArrival ImgSrc={item.image} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item._id} ratings={item.ratings} link2={""} information={false} safe={0} discount={0} />
+            )) : null}
+        </S.SearchBar>
+    )
+}
---
+++ src/components/Header/searchBar/searchBarStyle.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SearchBar = styled.div`
+    background-color: var(--Bright);
+    position: fixed;
+    display: flex;
+    max-height: 10vh;
+    right: 0;
+    overflow: auto;
+    
+`
