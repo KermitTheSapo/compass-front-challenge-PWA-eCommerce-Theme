2e2dd198c341d5da7c188c91f0e662f61c9eb64a
Robert <rjsf06@gmail.com>
2022-12-03 15:09:26 -0300

    :sparkles: feat: changing the wishlist component name to address

---
+++ src/components/address/address.tsx
@@ -0,0 +1,27 @@
+import * as S from "./addressStyle"
+import arrowBack from "@/assets/imgs/bag/back.svg"
+import wishlist from "@/assets/imgs/wishlist/art.svg"
+import { useNavigate } from "react-router-dom"
+import { Helmet } from "react-helmet"
+export default function Address() {
+    const navigate = useNavigate()
+    return (
+        <S.ConfirmedContainer>
+            <Helmet>
+                <title>Coral'l | WishList</title>
+            </Helmet>
+            <S.ConfirmedHeader>
+                <S.ImgBack src={arrowBack} alt="" onClick={() => navigate(-1)} />
+                <S.ConfirmedTitle>My Address Book</S.ConfirmedTitle>
+            </S.ConfirmedHeader>
+            <S.ConfirmedAdvise>
+                <S.ImgConfirmed src={wishlist} alt="" />
+                <S.ConfirmedAdviseTitle>No Address</S.ConfirmedAdviseTitle>
+                <S.ConfirmedAdviseText>It seems you have not updated your address yet.</S.ConfirmedAdviseText>
+            </S.ConfirmedAdvise>
+            <S.BtnActionsDiv>
+                <S.btnContinue>Add Address</S.btnContinue>
+            </S.BtnActionsDiv>
+        </S.ConfirmedContainer>
+    )
+}
