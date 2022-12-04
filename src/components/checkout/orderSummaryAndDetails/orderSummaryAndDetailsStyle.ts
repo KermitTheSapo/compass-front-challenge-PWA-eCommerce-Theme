7213dd5174ff0b9e1c39f874ba71dfc1ed0e4c4b
Robert <rjsf06@gmail.com>
2022-12-04 18:14:39 -0300

    :lipstick: feat: adding the emptyCart Style

--- src/components/checkout/orderSummaryAndDetails/orderSummaryAndDetailsStyle.ts
+++ src/components/checkout/orderSummaryAndDetails/orderSummaryAndDetailsStyle.ts
@@ -81,3 +81,23 @@ export const Separator = styled.div`
     height: 1px;
     background-color: var(--Separator);
 `
+
+export const EmptyCart = styled.div`
+    height: 20vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
+export const EmptyTitle = styled.h2`
+    font-weight: 700;
+    font-size: 1.75rem;
+    color: var(--Dark);
+`
+export const EmptyParagraph = styled.p`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--Dark);
+    width: 80%;
+    text-align: center;
+`
