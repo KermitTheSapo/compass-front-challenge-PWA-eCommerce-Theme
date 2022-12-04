c3c2441e660345e04e751e24240e5697b15e7268
Robert <rjsf06@gmail.com>
2022-12-04 18:07:13 -0300

    :sparkles: feat: doing the structure

--- src/components/singup/singUp.tsx
+++ src/components/singup/singUp.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react"
 import { Helmet } from "react-helmet"
 import * as S from "./singUpStyle"
 
 export default function SingUp() {
+    const [home, setHome] = useState(true)
+    const [login, setLogin] = useState(false)
+    const [singUp, setSingUp] = useState(false)
+
+
     return (
-        <S.SingUpContainer>
-            <Helmet>
-                <title>Coral'l | SingUp</title>
-            </Helmet>
-            <S.SingUpTitle>The shopping destination you need</S.SingUpTitle>
-            <button>Get Started</button>
-            <p>I already have an account</p>
-        </S.SingUpContainer>
+        <>
+            {home && <S.SingUpContainer>
+                <Helmet>
+                    <title>Coral'l | SingUp</title>
+                </Helmet>
+                <S.SingUpContent>
+                    <S.SingUpTitle>The shopping destination you need</S.SingUpTitle>
+                    <S.BtnGetStarted>Get Started</S.BtnGetStarted>
+                    <S.Paragraph onClick={() => { setLogin(true); setHome(false) }}>I already have an account</S.Paragraph>
+                </S.SingUpContent>
+            </S.SingUpContainer>}
+            {login && <S.LoginContainer>aaaaa</S.LoginContainer>}
+            {singUp && <h1>bbbbbbb</h1>}
+        </>
+
     )
 }
