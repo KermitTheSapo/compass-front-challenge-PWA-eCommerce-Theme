4dca3dad7625fe11dc78a7c7fa9374dab8584170
Robert <rjsf06@gmail.com>
2022-12-04 17:59:44 -0300

    :sparkles: feat: creating the refer structure and style component

---
+++ src/components/refer/refer.tsx
@@ -0,0 +1,34 @@
+import * as S from "./referStyle"
+
+import arrow from "@/assets/imgs/refer/arrow.svg"
+import art from "@/assets/imgs/refer/art.svg"
+
+export default function Refer() {
+    return (
+        <S.ReferContainer>
+            <S.ReferHeader>
+                <S.ImgArrow src={arrow} alt="" />
+                <S.ReferTitle>Refer & Earn</S.ReferTitle>
+            </S.ReferHeader>
+            <S.ReferBody>
+                <S.CodeContainer>
+                    <S.ImgArt src={art} alt="" />
+                    <S.Paragraph>Invite your friend and earn $20 on every invite</S.Paragraph>
+                    <S.DivCode>
+                        <S.Code>IVN6374730</S.Code>
+                    </S.DivCode>
+                    <S.ParagraphCode>Tap to copy the code.</S.ParagraphCode>
+                </S.CodeContainer>
+                <S.WorkDiv>
+                    <S.WorkTitle>How does this works?</S.WorkTitle>
+                    <S.WorkList>
+                        <S.WorkItem>How does this works?</S.WorkItem>
+                        <S.WorkItem>Ask your friends to place their order with your code & get $20 discount</S.WorkItem>
+                        <S.WorkItem>Once the order gets delivered you get the discount as well.</S.WorkItem>
+                    </S.WorkList>
+                </S.WorkDiv>
+                <S.InviteBtn>Invite now</S.InviteBtn>
+            </S.ReferBody>
+        </S.ReferContainer>
+    )
+}
---
+++ src/components/refer/referStyle.ts
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+
+export const ReferContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    
+`
+
+export const ReferHeader = styled.div`
+    display: flex;
+    align-items: center;
+    height: 8vh;
+    padding: 0 16px;
+`
+export const ImgArrow = styled.img`
+    margin-right: 20px;
+`
+export const ReferTitle = styled.h1`
+    font-weight: 600;
+    font-size: 1.25rem;
+    color: var(--Primary);
+`
+
+
+export const ReferBody = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items:center;
+
+`
+export const CodeContainer = styled.div`
+    height: 60vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+`
+
+export const ImgArt = styled.img`
+
+`
+export const Paragraph = styled.p`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--TypeHighEmphasis);
+    text-align: center;
+    width: 60%;
+`
+export const DivCode = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 40px;
+    width: 80%;
+    border-radius: 8px;
+    border: 1px dashed var(--Primary);
+`
+export const Code = styled.h3`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--TypeHighEmphasis);
+`
+export const ParagraphCode = styled.p`
+    font-weight: 500;
+    font-size: 0.75rem;
+    color: var(--TypeLowEmphasis);
+`
+export const WorkDiv = styled.div`
+    display: flex;
+    flex-direction: column;   
+    width: 95%;
+    height: 18vh;
+`
+export const WorkTitle = styled.h2`
+    font-weight: 600;
+    font-size: 0.875rem;
+    color: var(--TypeHighEmphasis);
+`
+export const WorkList = styled.ol`
+    display: flex; 
+    flex-direction: column;
+    height: 90%;
+    justify-content: center;    
+    gap: 20px;
+    list-style: disc;
+    padding: 0 25px;
+`
+export const WorkItem = styled.li`
+    font-weight: 500;
+    font-size: 14px;
+    color: var(--TypeLowEmphasis);
+`
+export const InviteBtn = styled.button`
+    width: 90%;
+    height: 44px;
+    border: none;
+    border-radius: 8px;
+    background-color: var(--Primary);
+    color: var(--Bright);
+    font-weight: 600;
+    font-size: 0.875rem;
+    position: fixed;
+    bottom: 2%;
+    left: 5%;
+`
