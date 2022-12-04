0f61448df7fa2d513bf992bd85687e388e7aa4a6
Robert <rjsf06@gmail.com>
2022-12-04 06:14:47 -0300

    :sparkles: feat: adding the SingUpTitle structure

--- src/components/singup/singUp.tsx
+++ src/components/singup/singUp.tsx
@@ -1,8 +1,12 @@
+import { Helmet } from "react-helmet"
 import * as S from "./singUpStyle"
 
 export default function SingUp() {
     return (
         <S.SingUpContainer>
+            <Helmet>
+                <title>Coral'l | SingUp</title>
+            </Helmet>
             <S.SingUpTitle>The shopping destination you need</S.SingUpTitle>
             <button>Get Started</button>
             <p>I already have an account</p>
