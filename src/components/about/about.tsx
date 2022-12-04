ff4b0ab28fd91d3f437941913fb335f32700ad86
Robert <rjsf06@gmail.com>
2022-12-04 18:03:26 -0300

    :sparkles: feat: creating the about structure and style component

---
+++ src/components/about/about.tsx
@@ -0,0 +1,48 @@
+import Footer from "../../components/footer/footer"
+import Header from "../../components/Header/Header"
+import bannerTwo from "@/assets/imgs/about/bannerTwo.png"
+import bannerThree from "@/assets/imgs/about/bannerThree.png"
+import bannerFour from "@/assets/imgs/about/bannerFour.png"
+import * as S from "./aboutStyle"
+
+export default function About() {
+    return (
+        <>
+            <Header />
+            <S.AboutContainer>
+                <S.BannerDiv>
+                    <S.BannerContent>
+                        <S.BannerTitle>About</S.BannerTitle>
+                        <S.BannerParagraph>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </S.BannerParagraph>
+                    </S.BannerContent>
+                </S.BannerDiv>
+                <S.AboutDiv>
+                    <S.AboutTitle>About</S.AboutTitle>
+                    <S.AboutParagraph>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </S.AboutParagraph>
+                </S.AboutDiv>
+                <S.Description>
+                    <S.DescriptionContent>
+                        <S.DescriptionTitle>About</S.DescriptionTitle>
+                        <S.DescriptionParagraph>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </S.DescriptionParagraph>
+                    </S.DescriptionContent>
+                    <S.ImgDescription src={bannerTwo} alt="" />
+                </S.Description>
+                <S.Description>
+                    <S.ImgDescription src={bannerThree} alt="" />
+                    <S.DescriptionContent>
+                        <S.DescriptionTitle>About</S.DescriptionTitle>
+                        <S.DescriptionParagraph>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </S.DescriptionParagraph>
+                    </S.DescriptionContent>
+                </S.Description>
+                <S.Description>
+                    <S.DescriptionContent>
+                        <S.DescriptionTitle>About</S.DescriptionTitle>
+                        <S.DescriptionParagraph>Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing.  Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum is simply dummy text of the printing. </S.DescriptionParagraph>
+                    </S.DescriptionContent>
+                    <S.ImgDescription src={bannerFour} alt="" />
+                </S.Description>
+            </S.AboutContainer>
+            <Footer />
+        </>
+    )
+}
---
+++ src/components/about/aboutStyle.ts
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import bannerOne from "@/assets/imgs/about/bannerOne.png"
+
+export const AboutContainer = styled.div`
+    margin-top: 56px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const BannerDiv = styled.div`
+    height: 401px;
+    background-image: url(${bannerOne});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 100%;
+`
+
+export const BannerContent = styled.div`
+    height: 50%;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`
+
+export const BannerTitle = styled.h1`
+    font-weight: 600;
+    font-size: 3.125rem;
+    line-height: 60.51px;
+    letter-spacing: 4.55px;
+    color: var(--Dark);
+    text-transform: uppercase;
+`
+export const BannerParagraph = styled.p`
+    font-weight: 400;
+    font-size: 1rem;
+    color: var(--TypeLowEmphasis);
+    width: 65%;
+    line-height: 20px;
+`
+
+export const AboutDiv = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 15vh;
+`
+
+export const AboutTitle = styled.h2`
+    font-weight: 600;
+    font-size: 1.25rem;
+    color: var(--TypeHighEmphasis);
+`
+export const AboutParagraph = styled.p`
+    font-weight: 400;
+    font-size: 1rem;
+    text-align: center;
+    line-height: 20px;
+    color: var(--TypeLowEmphasis);
+`
+
+export const Description = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-between;
+    height:40vh;
+`
+export const DescriptionContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 0 16px;
+`
+export const DescriptionTitle = styled.h2`
+    font-weight: 600;
+    font-size: 1.25rem;
+    color: var(--TypeHighEmphasis);
+`
+export const DescriptionParagraph = styled.p`
+    font-weight: 400;
+    font-size: 1rem;
+    color: var(--TypeLowEmphasis);
+    width: 85%;
+`
+export const ImgDescription = styled.img`
+
+`
