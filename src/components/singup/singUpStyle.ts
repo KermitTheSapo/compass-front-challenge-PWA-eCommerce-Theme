cd359af0365b57b424f15641ef7fc749f715a577
Robert <rjsf06@gmail.com>
2022-12-04 18:06:48 -0300

    :lipstick: feat: adding more style

--- src/components/singup/singUpStyle.ts
+++ src/components/singup/singUpStyle.ts
@@ -15,4 +15,36 @@ export const SingUpContainer = styled.div`
     width: 100vw;    
 `
 
-export const SingUpTitle = styled.h1``
+
+export const SingUpContent = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: column;
+    background: linear-gradient(180deg, rgba(255, 244, 229, 0) 0%, rgba(255, 250, 244, 0.96) 51.9%);
+    height: 30%;
+`
+export const SingUpTitle = styled.h1`
+    font-weight:700;
+    font-size: 1.875rem;
+    line-height: 42px;
+    color: var(--TypeHighEmphasis);
+    width: 90%;
+`
+export const BtnGetStarted = styled.button`
+    height: 44px;
+    width: 90%;
+    border: none;
+    border-radius: 8px;
+    background-color: var(--Primary);
+    font-weight: 600;
+    font-size: 0.875rem;
+    color: var(--Bright);
+`
+export const Paragraph = styled.p`
+    font-weight: 600;
+    font-size: 0.875rem;
+    color: var(--TypeHighEmphasis);
+`
+
+export const LoginContainer = styled.div``
