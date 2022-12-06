8f9243031db94a682fab0cc091270f06c9d662d4
Robert <rjsf06@gmail.com>
2022-12-06 13:39:09 -0300

    :sparkles: feat: adding new routes

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,10 @@ import MyReviews from "./components/myReviews/myReviews"
 import Refer from "./components/refer/refer"
 import About from "./components/about/about"
 import PhoneNumber from "./components/phoneNumber/phoneNumber"
+import EnterOtp from "./components/enterOtp/enterOtp"
+import MyOrders from "./components/myOrders/myOrders"
+import ItemsOrdered from "./components/itemsOrdered/itemsOrdered"
+import Invoice from "./components/invoice/invoice"
 
 export default function App() {
   return (
@@ -65,6 +69,10 @@ export default function App() {
         <Route path="/refer" element={<Refer />} />
         <Route path="/about" element={<About />} />
         <Route path="/phone-number" element={<PhoneNumber />} />
+        <Route path="/enter-otp" element={<EnterOtp />} />
+        <Route path="/my-orders" element={<MyOrders />} />
+        <Route path="/itemsOrdered" element={<ItemsOrdered />} />
+        <Route path="/invoices" element={<Invoice />} />
         <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
