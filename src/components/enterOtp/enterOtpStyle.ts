72bdfed72037b6151eac15ab9c53baf36587c74f
Robert <rjsf06@gmail.com>
2022-12-06 13:47:52 -0300

    :sparkles: feat: doing the enterOtp structure and style, and the input verification

---
+++ src/components/enterOtp/enterOtpStyle.ts
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const EnterOtpContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 0 16px;
+    height: 20vh;
+    justify-content: space-between;
+    margin-top: 52px;
+`
+export const Title = styled.h1`
+    font-weight: 700;
+    font-size: 2.5rem;
+    color: var(--Primary);
+`
+export const ParagraphInfo = styled.p`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--TypeLowEmphasis);
+`
+export const InputsDiv = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+`
+export const InputNumber = styled.input`
+    height: 42px;
+    width: 52px;
+    border-radius: 4px;
+    border: none;
+    background-color: var(--Grey);
+    padding: 0 0 0 8px;
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--TypeLowEmphasis);
+`
+export const RequestDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 68%;
+`
+export const CodeParagraph = styled.p`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--TypeLowEmphasis);
+`
+export const RequestParagraph = styled.p`
+    font-weight: 500;
+    font-size: 0.875rem;
+    color: var(--Primary);
+`
+export const CreateButton = styled.button`
+    height: 44px;
+    width: 90%;
+    border-radius: 8px;
+    border: none;
+    background-color: var(--Primary);
+    font-weight: 600;
+    font-size: 0.875rem;
+    color: var(--Bright);
+    position: fixed;
+    bottom: 5%;
+    left: 5%;
+`
