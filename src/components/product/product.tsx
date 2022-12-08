286e1f4e424f6a9821d5f84bb4361748cb48c6f3
Robert <rjsf06@gmail.com>
2022-12-08 11:30:27 -0300

    :sparkles: feat: removing the key in the map

--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -6,6 +6,7 @@ import { useLocation } from 'react-router-dom';
 
 
 export default function Product() {
+    // @ts-ignore
     let params = new URL(document.location).searchParams;
     let id = params.get("id");
     let location = useLocation();
@@ -29,7 +30,7 @@ export default function Product() {
 
     return (
         <>
-            {productsList && productsList.map((item, key) => (
+            {productsList && productsList.map((item) => (
                 <ProductPage name={item.name} img={item.image} discount={item.discount} productDescription="Experience comfortable and easy traveling like never before with this coach bag. It features a zip closure, removable straps and multiple organization compartments to keep your valuables safe. Crafted from premium material, it is durable and lasts long." productParagraph={item.paragraph} safe={item.safe} value={item.price} imgCarousel={item.image} ratings={item.ratings} />
             ))}
             <Helmet>
