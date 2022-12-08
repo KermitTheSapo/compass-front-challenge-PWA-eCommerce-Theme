bc0173d1a9ff1d783b5fec9dd604751c99fe8fce
Robert <rjsf06@gmail.com>
2022-12-08 20:21:58 -0300

    :sparkles: feat: doing the post review with the review informaions

--- src/components/productPage/addReview/addReview.tsx
+++ src/components/productPage/addReview/addReview.tsx
@@ -4,9 +4,54 @@ import upload from "@/assets/imgs/productPage/reviews/upload.svg"
 import { useNavigate } from "react-router-dom";
 import Ratings from "./star/star";
 import { Helmet } from "react-helmet";
+import { useEffect, useState } from "react";
+import { postReview } from "../../../products/review";
 
 export default function AddReview() {
     const navigate = useNavigate();
+    const [id, setId] = useState("")
+    const [starValue, setStarValue] = useState(0)
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [date, setDate] = useState("")
+    const getId = () => {
+        // @ts-ignore
+        let params = new URL(document.location).searchParams;
+        // @ts-ignore
+        setId(params.get("id"))
+    }
+    useEffect(() => {
+        getId()
+    }, [id])
+    useEffect(() => {
+        let now = new Date
+        const day = (now.getDate()).toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+        })
+        setDate(`${day}/${now.getMonth() + 1}/${now.getFullYear()}`)
+    }, [])
+
+    const submitReview = () => {
+        const review = {
+            userName: "Robert J.",
+            productId: id,
+            title: title,
+            date: date,
+            rating: starValue,
+            description: description,
+        }
+        if (starValue === 0) {
+            alert("Please enter the note for the product")
+        } else if (description.length < 2) {
+            alert("Please enter the description")
+        } else {
+            postReview(review)
+            setTitle("")
+            setDescription("")
+            alert("Review Added successfully")
+        }
+    }
     return (
         <S.AddReviewContainer>
             <Helmet>
@@ -18,15 +63,14 @@ export default function AddReview() {
             </S.HeaderNav>
             <S.ProductRatingDiv>
                 <S.ProductRatingTitle>Product Rating</S.ProductRatingTitle>
-                <Ratings />
+                <Ratings setState={setStarValue} />
                 <S.DeliveryPinCode>
-                    <S.DeliveryInput type="text" placeholder="Enter Valid PinCode" />
-                    <S.DeliveryBtn>Check</S.DeliveryBtn>
+                    <S.DeliveryInput type="text" placeholder="Enter the title" value={title} onChange={(e) => setTitle(e.target.value)} />
                 </S.DeliveryPinCode>
             </S.ProductRatingDiv>
             <S.ReviewDescriptionDiv>
                 <S.ReviewDescriptionTitle>Review Description</S.ReviewDescriptionTitle>
-                <S.ReviewDescriptionTextarea placeholder="Enter Description"></S.ReviewDescriptionTextarea>
+                <S.ReviewDescriptionTextarea placeholder="Enter Description" value={description} onChange={(e) => setDescription(e.target.value)}></S.ReviewDescriptionTextarea>
             </S.ReviewDescriptionDiv>
             <S.UploadDiv>
                 <S.UploadTitle>Upload Product Images</S.UploadTitle>
@@ -36,7 +80,7 @@ export default function AddReview() {
                 <S.InputUploadImg id="upload" type="file" />
             </S.UploadDiv>
             <S.ButtonSubmitDiv>
-                <S.ButtonSubmit>Submit Review</S.ButtonSubmit>
+                <S.ButtonSubmit onClick={() => submitReview()}>Submit Review</S.ButtonSubmit>
             </S.ButtonSubmitDiv>
         </S.AddReviewContainer >
     )
