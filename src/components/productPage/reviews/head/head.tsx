eaab694759d8692aa92749fdc20e3be2ec42a767
Robert <rjsf06@gmail.com>
2022-12-08 20:26:09 -0300

    :sparkles: feat: catching the product by id and using in the name and description product

--- src/components/productPage/reviews/head/head.tsx
+++ src/components/productPage/reviews/head/head.tsx
@@ -2,13 +2,51 @@ import * as S from "./headStyle"
 import star from "@/assets/imgs/productPage/reviews/star.svg"
 import img from "@/assets/imgs/productPage/reviews/image.png"
 import Stats from "./stats/stats"
+import { getProductsById } from "../../../../products/products"
+import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
 
-export default function Head() {
+type Props = {
+    id: string;
+    state: {
+        date: string;
+        description: string;
+        productId: string;
+        rating: string;
+        title: string;
+        userName: string;
+    }[];
+}
+
+export default function Head({ id, state }: Props) {
+    let location = useLocation();
+    const [productsList, setProductsList] = useState([{
+        _id: "",
+        name: "",
+        price: 0,
+        category: "",
+        image: "",
+        description: "",
+        imgAlt: "",
+        paragraph: "",
+        link: "",
+        ratings: 0,
+        discount: 0,
+        safe: 0
+    }])
+    useEffect(() => {
+        getProductsById(id).then((res) => setProductsList([res]))
+    }, [location, id])
+
     return (
         <S.HeadContainer>
             <S.ProductHead>
-                <S.ProductHeadTitle>Boujee</S.ProductHeadTitle>
-                <S.ProductHeadSubTitle>Baker Solid Black Washable Shoulder Bag</S.ProductHeadSubTitle>
+                {productsList && productsList.map((item) => (
+                    <>
+                        <S.ProductHeadTitle>{item.name}</S.ProductHeadTitle>
+                        <S.ProductHeadSubTitle>{item.paragraph}</S.ProductHeadSubTitle>
+                    </>
+                ))}
             </S.ProductHead>
             <S.Ratings>
                 <S.AverageRatings>
@@ -16,7 +54,7 @@ export default function Head() {
                     <S.AverageRatingsStar src={star} alt="star icon" />
                     <S.AverageTitle>Average Rating</S.AverageTitle>
                 </S.AverageRatings>
-                <Stats />
+                <Stats id={id} state={state} />
             </S.Ratings>
             <S.Photos>
                 <S.PhotosTitle>Customer Photos</S.PhotosTitle>
