924be4b009feac5b2df917cc88e83bad04865dd8
Robert <rjsf06@gmail.com>
2022-12-08 20:24:55 -0300

    :sparkles: feat: changing the navigate to a route with id parameters

--- src/components/productPage/ratingsReviews/ratingReviews.tsx
+++ src/components/productPage/ratingsReviews/ratingReviews.tsx
@@ -2,12 +2,16 @@ import * as S from "./ratingReviewsStyle"
 import arrowRight from "@/assets/imgs/productPage/arrow-right.svg"
 import { useNavigate } from "react-router-dom"
 
-export default function RatingReviews() {
+type Props = {
+    id: string;
+}
+
+export default function RatingReviews({ id }: Props) {
     const navigate = useNavigate()
     return (
         <S.RatingsReviews>
             <S.RatingsReviewsTitle>Ratings & Reviews</S.RatingsReviewsTitle>
-            <S.LinkRatings onClick={() => navigate("/reviews")}><S.RatingArrow src={arrowRight} alt="an arrow to the right" /></S.LinkRatings>
+            <S.LinkRatings onClick={() => navigate(`/reviews?id=${id}`)}><S.RatingArrow src={arrowRight} alt="an arrow to the right" /></S.LinkRatings>
         </S.RatingsReviews>
     )
 }
