6d5899181f842eaf58b16fe0382b0b4b952e3d2c
Robert <rjsf06@gmail.com>
2022-12-08 20:29:27 -0300

    :sparkles: feat: using the rating component to be used with map and passing the informations with props

--- src/components/productPage/reviews/ratings/ratings.tsx
+++ src/components/productPage/reviews/ratings/ratings.tsx
@@ -1,22 +1,32 @@
 import * as S from "./ratingsStyle"
 import star from "@/assets/imgs/productPage/reviews/star.svg"
 import img from "@/assets/imgs/productPage/reviews/image.png"
-export default function Ratings() {
+
+type Props = {
+    date: string;
+    description: string;
+    productId: string;
+    rating: string;
+    title: string;
+    userName: string;
+}
+
+export default function Ratings({ date, description, productId, rating, title, userName }: Props) {
     return (
         <S.RatingsContainer>
             <S.HeaderInformation>
                 <S.RatingsPill>
-                    <S.RatingNumber>4.0</S.RatingNumber>
+                    <S.RatingNumber>{rating}.0</S.RatingNumber>
                     <S.StarImg src={star} alt="star icon" />
                 </S.RatingsPill>
                 <S.RatingsPersonInfo>
-                    <S.RatingsPersonName>John Doe</S.RatingsPersonName>
-                    <S.RatingsPersonDate>12/12/2020</S.RatingsPersonDate>
+                    <S.RatingsPersonName>{userName}</S.RatingsPersonName>
+                    <S.RatingsPersonDate>{date}</S.RatingsPersonDate>
                 </S.RatingsPersonInfo>
             </S.HeaderInformation>
             <S.Content>
-                <S.RatingsTitle>Must go for the class feel.</S.RatingsTitle>
-                <S.RatingsDescription>Totally amazing! I loved the material and the quality. It has a jolly vibe in it which makes me feel happy every time I put it on.</S.RatingsDescription>
+                <S.RatingsTitle>{title}</S.RatingsTitle>
+                <S.RatingsDescription>{description}</S.RatingsDescription>
             </S.Content>
             <S.ImgsCarousel>
                 <S.ImgCarousel src={img} alt="product img" />
