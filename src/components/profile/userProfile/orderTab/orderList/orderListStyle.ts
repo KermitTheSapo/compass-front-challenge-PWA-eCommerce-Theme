081a35028e143f235c7b664e2acaecbe090ceb67
Robert <rjsf06@gmail.com>
2022-12-08 11:16:37 -0300

    :sparkles: feat: using orderTab structure and style and puttin in the orderList component

---
+++ src/components/profile/userProfile/orderTab/orderList/orderListStyle.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+
+export const OrderTabs = styled.div`
+    display: flex;
+    align-items: center;
+    background-color: var(--Grey);
+    border-radius: 12px;
+    height: 10vh;
+    gap: 24px;
+    padding: 0px 12px;
+`
+export const OrderItem = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50%;
+    width: 121px;
+    background-color: ${p => p.color};
+    border-radius: 8px;
+`
+export const OrderItemLabel = styled.p`
+    font-weight: 500;
+    font-size: 1rem;
+    color: ${p => p.color};
+`
+
+export const OrderList = styled.div``
+export const OrderHeader = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 5vh;
+    width: 90%;
+`
+export const OrderHeaderLabel = styled.p`
+    font-weight: 500;
+    font-size: 1rem;
+    color: var(--TypeLowEmphasis);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 25%;
+`
+
+export const Separator = styled.div`
+    width: 100%;
+    height: 1px;
+    background-color: var(--Separator);
+`
+
+export const OrderProducts = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 17px;
+    margin-top: 24px;
+    height: 60vh;
+`
