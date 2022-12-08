1703a618fdc84187e7a54b09faf7a62de12f7311
Robert <rjsf06@gmail.com>
2022-12-08 20:21:00 -0300

    :sparkles: feat: catching the order informations and doing a post in the api

--- src/components/payments/payments.tsx
+++ src/components/payments/payments.tsx
@@ -1,13 +1,16 @@
 import Address from "../../components/order/address/address"
 import BottomSheetAddress from "../../components/order/bottomSheetAddress/bottomSheetAddress"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import * as S from "./paymentsStyle"
 import arrow from "@/assets/imgs/newAddress/arrow.svg"
 import arrowDown from "@/assets/imgs/payments/arrow-down.svg"
 import arrowUp from "@/assets/imgs/payments/arrow-up.svg"
 import PaymentMethod from "./paymentMethod/paymentMethod"
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import { Helmet } from "react-helmet"
+import { getAddress } from "../../products/address"
+import { getBag } from "../../products/bag"
+import { postOrder } from "../../products/order"
 
 export default function Payments() {
     const [sheetEditAddress, setSheetEditAddress] = useState(false)
@@ -15,12 +18,67 @@ export default function Payments() {
     const [upiText, setUpiText] = useState("")
     const [correctInfo, setCorrectInfo] = useState(false)
     const navigate = useNavigate()
+    const [date, setDate] = useState("")
+    const [productsList, setProductsList] = useState([{
+        _id: "",
+        name: "",
+        price: 0,
+        category: "",
+        image: "",
+        description: "",
+        imgAlt: "",
+        paragraph: "",
+        link: "",
+        ratings: 0,
+        discount: 0,
+        safe: 0,
+        quantity: 0
+    }])
+    // @ts-ignore
+    let params = new URL(document.location).searchParams;
+    let total = params.get("total") as string;
+    let subTotal = params.get("subtotal") as string;
+    // let location = useLocation();
+    const [addressList, setAddressList] = useState([{
+        _id: "",
+        streetAddress: "",
+        city: "",
+        uf: "",
+        pinCode: 0,
+    }])
 
+    useEffect(() => {
+        getAddress().then((res) => setAddressList(res))
+        getBag().then((res) => setProductsList(res))
+    }, [])
     const checkInformation = () => {
         if (upiText.length > 7) {
-            navigate("/confirmed")
             setCorrectInfo(false)
+            let orderId = (Math.floor(Math.random() * 1000000000)).toLocaleString('en-US', {
+                minimumIntegerDigits: 9,
+                useGrouping: false
+            })
+            let now = new Date
+            const day = (now.getDate()).toLocaleString('en-US', {
+                minimumIntegerDigits: 2,
+                useGrouping: false
+            })
+            setDate(`${day}/${now.getMonth() + 1}/${now.getFullYear()}`)
+            const Order = {
+                name: "Robert J.",
+                orderId: orderId,
+                total: total,
+                subTotal: subTotal,
+                upi: upiText,
+                addressList: addressList[0],
+                product: productsList,
+                orderDate: date
+            }
+            postOrder(Order)
+            alert("successful purchase")
+            // navigate("/confirmed")
         } else {
+            alert("write your upi")
             setCorrectInfo(true)
         }
     }
@@ -50,7 +108,7 @@ export default function Payments() {
             <S.BtnPayNowDiv>
                 <S.BtnPay onClick={() => { checkInformation() }}>Pay Now</S.BtnPay>
             </S.BtnPayNowDiv>
-            {sheetEditAddress && <BottomSheetAddress state={sheetEditAddress} setState={setSheetEditAddress} />}
+            {sheetEditAddress && <BottomSheetAddress state={sheetEditAddress} setState={setSheetEditAddress} stateAddress={addressList} />}
         </S.PaymentsContainer>
     )
 }
