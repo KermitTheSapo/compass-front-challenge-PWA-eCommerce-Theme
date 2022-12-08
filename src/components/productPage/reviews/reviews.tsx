7b21529ab914d1ef887218da59f0501da0bb6988
Robert <rjsf06@gmail.com>
2022-12-08 20:30:46 -0300

    :sparkles: feat: doing the getReview and doing the reviewFilter and using in the ratings Component

--- src/components/productPage/reviews/reviews.tsx
+++ src/components/productPage/reviews/reviews.tsx
@@ -1,16 +1,43 @@
 import * as S from "./reviewsStyle"
 
 import arrowLeft from "@/assets/imgs/productPage/reviews/arrowLeft.svg"
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Head from "./head/head";
 import Ratings from "./ratings/ratings";
 import { Helmet } from "react-helmet";
-
-
+import { useEffect, useState } from "react";
+import { getReview } from "../../../products/review";
 
 export default function Reviews() {
     const navigate = useNavigate();
-
+    // @ts-ignore
+    let params = new URL(document.location).searchParams;
+    let id = params.get("id") as string;
+    let location = useLocation();
+    const [reviewList, setReviewList] = useState([{
+        date: "",
+        description: "",
+        productId: "",
+        rating: "",
+        title: "",
+        userName: ""
+    }])
+    const [reviewFilterList, setReviewFilterList] = useState([{
+        date: "",
+        description: "",
+        productId: "",
+        rating: "",
+        title: "",
+        userName: ""
+    }])
+    useEffect(() => {
+        getReview().then((res) => setReviewList(res))
+    }, [location])
+    useEffect(() => {
+        if (reviewList.length !== 0) {
+            setReviewFilterList(reviewList.filter((item) => item.productId.includes(id)))
+        }
+    }, [reviewList, location])
     return (
         <S.ReviewsContainer>
             <Helmet>
@@ -19,15 +46,15 @@ export default function Reviews() {
             <S.HeaderNav>
                 <S.ImgNavArrow onClick={() => navigate(-1)} src={arrowLeft} alt="left arrow icon" />
             </S.HeaderNav>
-            <Head />
+            <Head id={id} state={reviewFilterList} />
             <S.Separator></S.Separator>
-            <Ratings />
-            <Ratings />
-            <Ratings />
-            <Ratings />
-            <Ratings />
+            <S.ReviewDiv>
+                {reviewFilterList && reviewFilterList.map((item) => (
+                    <Ratings date={item.date} description={item.description} productId={item.productId} rating={item.rating} title={item.title} userName={item.userName} />
+                ))}
+            </S.ReviewDiv>
             <S.ButtonAdd>
-                <S.ButtonAddText onClick={() => navigate("/addReview")}>Write a Review</S.ButtonAddText>
+                <S.ButtonAddText onClick={() => navigate(`/addReview?id=${id}`)}>Write a Review</S.ButtonAddText>
             </S.ButtonAdd>
         </S.ReviewsContainer>
     )
