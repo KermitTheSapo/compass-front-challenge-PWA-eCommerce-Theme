34cdfe83a669770872ee90943e177d3c97ae26b2
Robert <rjsf06@gmail.com>
2022-12-09 20:21:00 -0300

    :sparkles: feat: doing the savedCards structure

---
+++ src/components/profile/userProfile/savedCards/savedCards.tsx
@@ -0,0 +1,9 @@
+import * as S from "./savedCardsStyle"
+
+export default function SavedCards() {
+    return (
+        <S.SavedCardsContainer>
+            <S.Title>Error 404 - page not found</S.Title>
+        </S.SavedCardsContainer>
+    )
+}
---
+++ src/components/profile/userProfile/savedCards/savedCardsStyle.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SavedCardsContainer = styled.div`
+    height: 55vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Title = styled.div`
+    font-weight: 700;
+    font-size: 1.75rem;
+    color: var(--Dark);
+`
