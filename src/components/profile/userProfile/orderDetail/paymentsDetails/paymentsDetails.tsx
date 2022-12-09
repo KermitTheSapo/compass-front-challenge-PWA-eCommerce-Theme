b9a0eabaa8acce5c4851479e5059c504477ce7a9
Robert <rjsf06@gmail.com>
2022-12-09 12:54:33 -0300

    :sparkles: feat: changing the structure to be used with type PRops

--- src/components/profile/userProfile/orderDetail/paymentsDetails/paymentsDetails.tsx
+++ src/components/profile/userProfile/orderDetail/paymentsDetails/paymentsDetails.tsx
@@ -1,11 +1,15 @@
 import * as S from "./paymentsDetailsStyle"
 
-export default function PaymentsDetails() {
+type Props = {
+    payment: string;
+}
+
+export default function PaymentsDetails({payment}: Props) {
     return (
         <S.PaymentsDetailsContainer>
             <S.PaymentsDetailsTitle>Payment Details</S.PaymentsDetailsTitle>
             <S.PaymentsDetailsContent>
-                <S.Paragraph>Cash on Delivery</S.Paragraph>
+                <S.Paragraph>Upi: {payment}</S.Paragraph>
             </S.PaymentsDetailsContent>
         </S.PaymentsDetailsContainer>
     )
