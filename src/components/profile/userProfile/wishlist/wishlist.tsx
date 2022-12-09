fb308a1bb4952c0d9d743ac9ca12215795dd088b
Robert <rjsf06@gmail.com>
2022-12-09 20:21:41 -0300

    :sparkles: feat: doing the wishlist map in the desktop

---
+++ src/components/profile/userProfile/wishlist/wishlist.tsx
@@ -0,0 +1,41 @@
+import { getWishlist } from "../../../../products/wishlist"
+import { useEffect, useState } from "react"
+import * as S from "./wishlistStyle"
+import ItemArrival from "../../../../components/Home/main/Arrivals/itemArrival/itemArrival"
+import WishlistError from "../../../../components/wishlist/wishlistError/wishlistError"
+import stars from "@/assets/imgs/home/main/arrivals/stars.svg"
+export default function WishlistPage() {
+    const [productsList, setProductsList] = useState([{
+        _id: "",
+        name: "",
+        price: 0,
+        category: "",
+        image: "",
+        description: "",
+        imgAlt: "",
+        paragraph: "",
+        link: "",
+        ratings: 0,
+        discount: 0,
+        safe: 0
+    }])
+
+    useEffect(() => {
+        getWishlist().then((res) => setProductsList(res))
+    }, [])
+    return (
+        <S.WishlistPage>
+            <S.WishlistParagraph>{productsList.length} Product(s)</S.WishlistParagraph>
+            <S.ItemList>
+
+                {productsList.map((item, key) => (
+                    <S.ItemDiv>
+                        <ItemArrival key={key} ImgSrc={item.image} ImgAlt={item.imgAlt} itemName={item.name} Description={item.paragraph} Price={item.price} link={item._id} safe={item.safe} discount={item.discount} star={stars} ratings={item.ratings} information={true} isButtonAddTrue={true} />
+                    </S.ItemDiv>
+                ))}
+
+            </S.ItemList>
+            {productsList.length === 0 && <WishlistError />}
+        </S.WishlistPage>
+    )
+}
---
+++ src/components/profile/userProfile/wishlist/wishlistStyle.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const WishlistPage = styled.div`
+
+`
+export const WishlistParagraph = styled.p`
+    font-weight: 600;
+    font-size: 0.875rem;
+    color: var(--TypeLowEmphasis);
+`
+export const ItemDiv = styled.div`
+    width: 30%;
+`
+export const ItemList = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
