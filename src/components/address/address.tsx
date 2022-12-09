368c9d0dfdb2e24eeee516143d6bc40c1d1558f1
Robert <rjsf06@gmail.com>
2022-12-09 20:16:20 -0300

    :sparkles: feat: adding the addressList using the getAddress

--- src/components/address/address.tsx
+++ src/components/address/address.tsx
@@ -3,8 +3,22 @@ import arrowBack from "@/assets/imgs/bag/back.svg"
 import address from "@/assets/imgs/address/art.svg"
 import { useNavigate } from "react-router-dom"
 import { Helmet } from "react-helmet"
+import AddressCard from "../../components/order/bottomSheetAddress/addressCard/addressCard"
+import { useEffect, useState } from "react"
+import { getAddress } from "../../products/address"
+
 export default function Address() {
     const navigate = useNavigate()
+    const [addressList, setAddressList] = useState([{
+        _id: "",
+        streetAddress: "",
+        city: "",
+        uf: "",
+        pinCode: 0,
+    }])
+    useEffect(() => {
+        getAddress().then((res) => setAddressList(res))
+    }, [])
     return (
         <S.ConfirmedContainer>
             <Helmet>
@@ -14,13 +28,19 @@ export default function Address() {
                 <S.ImgBack src={arrowBack} alt="an arrow to the left" onClick={() => navigate(-1)} />
                 <S.ConfirmedTitle>My Address Book</S.ConfirmedTitle>
             </S.ConfirmedHeader>
-            <S.ConfirmedAdvise>
-                <S.ImgConfirmed src={address} alt="icon of a location point with a circle indicating error" />
-                <S.ConfirmedAdviseTitle>No Address</S.ConfirmedAdviseTitle>
-                <S.ConfirmedAdviseText>It seems you have not updated your address yet.</S.ConfirmedAdviseText>
-            </S.ConfirmedAdvise>
+            {
+                addressList.length < 0 ? <S.ConfirmedAdvise>
+                    <S.ImgConfirmed src={address} alt="icon of a location point with a circle indicating error" />
+                    <S.ConfirmedAdviseTitle>No Address</S.ConfirmedAdviseTitle>
+                    <S.ConfirmedAdviseText>It seems you have not updated your address yet.</S.ConfirmedAdviseText>
+                </S.ConfirmedAdvise> : <S.AddressList>
+                    {addressList.map((item) => (
+                        <AddressCard streetAddress={item.streetAddress} city={item.city} uf={item.uf} pinCode={item.pinCode} />
+                    ))}
+                </S.AddressList>
+            }
             <S.BtnActionsDiv>
-                <S.btnContinue>Add Address</S.btnContinue>
+                <S.btnContinue onClick={() => navigate("/new-address")}>Add Address</S.btnContinue>
             </S.BtnActionsDiv>
         </S.ConfirmedContainer>
     )
