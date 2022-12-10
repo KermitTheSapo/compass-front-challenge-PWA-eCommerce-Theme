40a8dc5c537adffae94ff4a3d5eea4b6cc4117df
Robert <rjsf06@gmail.com>
2022-12-10 13:22:41 -0300

    :sparkles: feat: changing the structure

--- src/components/profile/userProfile/orderTab/orderItem/orderItem.tsx
+++ src/components/profile/userProfile/orderTab/orderItem/orderItem.tsx
@@ -15,16 +15,18 @@ export default function OrderItem({ id, orderId, date, price, status }: Props) {
     const navigate = useNavigate();
     return (
         <S.OrderItemContainer>
-            <S.OrderId>
-                <S.InputCheckbox type="checkbox" name="" id="" />
-                <S.Label>#{orderId}</S.Label>
-            </S.OrderId>
-            <S.DateDiv>
-                <S.Label>{date}</S.Label>
-            </S.DateDiv>
-            <S.PriceDiv>
-                <S.Label>{price}</S.Label>
-            </S.PriceDiv>
+            <S.OrderCardContent>
+                <S.OrderId>
+                    <S.InputCheckbox type="checkbox" name="" id="" />
+                    <S.Label>#{orderId}</S.Label>
+                </S.OrderId>
+                <S.DateDiv>
+                    <S.LabelDate>{date}</S.LabelDate>
+                </S.DateDiv>
+                <S.PriceDiv>
+                    <S.Label>${price}</S.Label>
+                </S.PriceDiv>
+            </S.OrderCardContent>
             <S.StatusDiv>
                 <S.StatusParagraph>{status}</S.StatusParagraph>
             </S.StatusDiv>
