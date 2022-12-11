155147d9a59fb5775d59bbbe3aba7359df0b40a3
Robert <rjsf06@gmail.com>
2022-12-11 17:25:13 -0300

    :sparkles: feat: adding typeProps and ratings

--- src/components/myReviews/reviewCard/reviewCard.tsx
+++ src/components/myReviews/reviewCard/reviewCard.tsx
@@ -3,22 +3,70 @@ import arrowOn from "@/assets/imgs/myReviews/arrowRight.svg"
 import starOn from "@/assets/imgs/myReviews/starOn.svg"
 import starOff from "@/assets/imgs/myReviews/starOff.svg"
 
+type Props = {
+    date: string,
+    title: string,
+    paragraph: string,
+    description: string,
+    rating: string
+}
 
-export default function ReviewCard() {
+export default function ReviewCard({ date, title, paragraph, description, rating }: Props) {
     return (
         <S.ReviewCard>
             <S.ReviewCardContent>
-                <S.Date>12 August 2020</S.Date>
+                <S.Date>{date}</S.Date>
                 <S.StarDiv>
-                    <img src={starOn} alt="yellow star icon" />
-                    <img src={starOn} alt="yellow star icon" />
-                    <img src={starOn} alt="yellow star icon" />
-                    <img src={starOn} alt="yellow star icon" />
-                    <img src={starOff} alt="star off icon" />
+                    {
+                        rating === "1" && <>
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOff} alt="yellow star icon" />
+                            <img src={starOff} alt="yellow star icon" />
+                            <img src={starOff} alt="yellow star icon" />
+                            <img src={starOff} alt="star off icon" />
+                        </>
+                    }
+                    {
+                        rating === "2" && <>
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOff} alt="yellow star icon" />
+                            <img src={starOff} alt="yellow star icon" />
+                            <img src={starOff} alt="star off icon" />
+                        </>
+                    }
+                    {
+                        rating === "3" && <>
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOff} alt="yellow star icon" />
+                            <img src={starOff} alt="star off icon" />
+                        </>
+                    }
+                    {
+                        rating === "4" && <>
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOff} alt="star off icon" />
+                        </>
+                    }
+                    {
+                        rating === "5" && <>
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOn} alt="yellow star icon" />
+                            <img src={starOn} alt="star off icon" />
+                        </>
+                    }
+
                 </S.StarDiv>
-                <S.Title>GUCCI Colbar Handbag</S.Title>
-                <S.Paragraph>Quality product with awesome accessories</S.Paragraph>
-                <S.Description>Quality product with awesome accessories Quality product accessories ......</S.Description>
+                <S.Title>{title}</S.Title>
+                <S.Paragraph>{paragraph}</S.Paragraph>
+                <S.Description>{description}</S.Description>
             </S.ReviewCardContent>
             <S.ImgArrow src={arrowOn} alt="right arrow" />
         </S.ReviewCard>
