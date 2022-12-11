93c71161a16db30c6dd88034e4e56af279f51a6a
Robert <rjsf06@gmail.com>
2022-12-11 17:28:19 -0300

    :sparkles: feat: making the item be delet when finish the order

--- src/components/payments/payments.tsx
+++ src/components/payments/payments.tsx
@@ -9,8 +9,9 @@ import PaymentMethod from "./paymentMethod/paymentMethod"
 import { useNavigate } from "react-router-dom"
 import { Helmet } from "react-helmet"
 import { getAddress } from "../../products/address"
-import { getBag } from "../../products/bag"
+import { deleteBag, getBag } from "../../products/bag"
 import { postOrder } from "../../products/order"
+import { getContact } from "../../products/contact"
 
 export default function Payments() {
     const [sheetEditAddress, setSheetEditAddress] = useState(false)
@@ -39,7 +40,6 @@ export default function Payments() {
     let params = new URL(document.location).searchParams;
     let total = params.get("total") as string;
     let subTotal = params.get("subtotal") as string;
-    // let location = useLocation();
     const [addressList, setAddressList] = useState([{
         _id: "",
         streetAddress: "",
@@ -47,10 +47,15 @@ export default function Payments() {
         uf: "",
         pinCode: 0,
     }])
+    const [contact, setContact] = useState({
+        firstName: "",
+        LastName: ""
+    })
 
     useEffect(() => {
         getAddress().then((res) => setAddressList(res))
         getBag().then((res) => setProductsList(res))
+        getContact().then(res => setContact(res[res.length - 1]))
         let now = new Date
         const day = (now.getDate()).toLocaleString('en-US', {
             minimumIntegerDigits: 2,
@@ -67,7 +72,7 @@ export default function Payments() {
                 useGrouping: false
             })
             const Order = {
-                name: "Robert J.",
+                name: contact.firstName + " " + contact.LastName,
                 orderId: orderId,
                 total: total,
                 subTotal: subTotal,
@@ -78,6 +83,9 @@ export default function Payments() {
                 status: "Paid"
             }
             postOrder(Order)
+            productsList.map((product) => {
+                deleteBag(product._id)
+            })
             alert("successful purchase")
             navigate("/confirmed")
         } else {
