3a621ce8618ade42469499bf87cf8601e99e5968
Robert <rjsf06@gmail.com>
2022-12-11 07:25:14 -0300

    :sparkles: feat: doing the getContact and maskPhone and using the informations

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,16 +1,28 @@
 import * as S from "./profileStyle"
-
-import imgProfile from "@/assets/imgs/profile/imgProfile.png"
 import arrowRightGrey from "@/assets/imgs/profile/arrowRightGrey.svg"
 import SideNavigation from "./sideNavigation/sideNavigation"
 import Navigation from "../../components/Home/main/bottom-navigation/navigation"
 import { Helmet } from "react-helmet"
 import UserProfile from "./userProfile/userProfile"
 import { useEffect, useState } from "react"
+import { getContact } from "../../products/contact"
+import { useNavigate } from "react-router-dom"
 
 
 export default function Profile() {
     const [size, setSize] = useState(false)
+    const navigate = useNavigate()
+    const [contact, setContact] = useState({
+        _id: "",
+        ddd: "",
+        phone: "",
+        firstName: "",
+        LastName: "",
+        email: "",
+        dateBirth: "",
+        password: "",
+        image: ""
+    })
     useEffect(() => {
         if (window.screen.width > 768) {
             setSize(true)
@@ -25,6 +37,16 @@ export default function Profile() {
             }
         })
     }, [size])
+    const maskPhone = (value: string) => {
+        return value
+            .replace(/\D/g, "")
+            .replace(/(\d{2})(\d)/, "($1) $2")
+            .replace(/(\d{5})(\d)/, "$1-$2")
+            .replace(/(-\d{4})(\d+?)$/, "$1");
+    };
+    useEffect(() => {
+        getContact().then(res => setContact(res[res.length - 1]))
+    }, [])
     return (
         <S.ProfileContainer>
             <Helmet>
@@ -34,13 +56,13 @@ export default function Profile() {
                 <S.ProfileTitle>Profile</S.ProfileTitle>
             </S.ProfileHeader>
             <S.ProfileBanner>
-                <S.ImgProfile src={imgProfile} alt="profile img" />
+                <S.ImgProfile src={contact.image} alt="profile img" />
                 <S.ProfileContent>
-                    <S.ProfileName>Tina Vargayee</S.ProfileName>
-                    <S.ProfileEmail>tinavar@vinho.com</S.ProfileEmail>
-                    <S.ProfileNumber>+85-5478564</S.ProfileNumber>
+                    <S.ProfileName>{`${contact.firstName} ${contact.LastName}`}</S.ProfileName>
+                    <S.ProfileEmail>{contact.email}</S.ProfileEmail>
+                    <S.ProfileNumber>{`+${contact.ddd} ${maskPhone(contact.phone)}`}</S.ProfileNumber>
                 </S.ProfileContent>
-                <S.ImgArrow src={arrowRightGrey} alt="right arrow" />
+                <S.ImgArrow src={arrowRightGrey} alt="right arrow" onClick={() => navigate("/personal")} />
             </S.ProfileBanner>
             <S.NavigationOptions>
                 <SideNavigation path={"/personal"} name={"Personal Information"} />
