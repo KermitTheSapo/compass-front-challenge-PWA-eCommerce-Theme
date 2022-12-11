b9b43e2c6d11e5434df41a4eeee3801c26c353e9
Robert <rjsf06@gmail.com>
2022-12-11 17:27:20 -0300

    :sparkles: feat: doing the getAddress and contact in the informations

--- src/components/order/bottomSheetAddress/addressCard/addressCard.tsx
+++ src/components/order/bottomSheetAddress/addressCard/addressCard.tsx
@@ -1,3 +1,5 @@
+import { getContact } from "../../../../products/contact";
+import { useEffect, useState } from "react";
 import * as S from "./addressCardStyle"
 type Props = {
     streetAddress: string;
@@ -6,12 +8,19 @@ type Props = {
     pinCode: Number;
 }
 export default function AddressCard({ streetAddress, city, uf, pinCode }: Props) {
+    const [contact, setContact] = useState({
+        firstName: "",
+        LastName: ""
+    })
+    useEffect(() => {
+        getContact().then(res => setContact(res[res.length - 1]))
+    }, [])
     return (
         <S.AddressCardContainer>
             <S.CardContent>
                 <S.InputRadio type="radio" name="address" id="home" />
                 <S.Label htmlFor="home">
-                    <S.AddressCardName>Vincent Lobo</S.AddressCardName>
+                    <S.AddressCardName>{`${contact.firstName === "" ? "unknown" : contact.firstName} ${contact.LastName === "" ? "" : contact.LastName}`}</S.AddressCardName>
                     <S.AddressCardAddress>{streetAddress}</S.AddressCardAddress>
                     <S.AddressCardCity>{city} - {uf}</S.AddressCardCity>
                     <S.AddressCardPhone>{`${pinCode}`}</S.AddressCardPhone>
