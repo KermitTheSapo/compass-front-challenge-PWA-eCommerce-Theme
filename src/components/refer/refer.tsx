cb50278a3f26eed2e3dd1637334e1e8aaf358538
Robert <rjsf06@gmail.com>
2022-12-11 07:27:46 -0300

    :sparkles: feat: adding the copy to clipboard function

--- src/components/refer/refer.tsx
+++ src/components/refer/refer.tsx
@@ -3,10 +3,25 @@ import * as S from "./referStyle"
 import arrow from "@/assets/imgs/refer/arrow.svg"
 import art from "@/assets/imgs/refer/art.svg"
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Helmet } from "react-helmet";
 export default function Refer() {
     const navigate = useNavigate();
+    const [code, setCode] = useState("IVN6374730")
+    const [msg, setMsg] = useState(false)
+
+    const copy = () => {
+        navigator.clipboard.writeText(`${code}`)
+        setMsg(true)
+        setTimeout(() => {
+            setMsg(false)
+        }, 5000)
+    }
     return (
         <S.ReferContainer>
+            <Helmet>
+                <title>Coral'l | Refer</title>
+            </Helmet>
             <S.ReferHeader>
                 <S.ImgArrow src={arrow} alt="left arrow" onClick={() => navigate(-1)} />
                 <S.ReferTitle>Refer & Earn</S.ReferTitle>
@@ -15,9 +30,12 @@ export default function Refer() {
                 <S.CodeContainer>
                     <S.ImgArt src={art} alt="icon of a receipt and an okay in the middle" />
                     <S.Paragraph>Invite your friend and earn $20 on every invite</S.Paragraph>
-                    <S.DivCode>
-                        <S.Code>IVN6374730</S.Code>
+                    <S.DivCode onClick={() => copy()}>
+                        <S.Code>{code}</S.Code>
                     </S.DivCode>
+                    {
+                        msg && <S.AlertCopyMsg>Copiado com sucesso!</S.AlertCopyMsg>
+                    }
                     <S.ParagraphCode>Tap to copy the code.</S.ParagraphCode>
                 </S.CodeContainer>
                 <S.WorkDiv>
