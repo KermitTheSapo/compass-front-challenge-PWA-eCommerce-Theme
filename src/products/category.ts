66637e55bd32cc5a9145e52417f7b26992c96fa3
Robert <rjsf06@gmail.com>
2022-12-14 16:29:54 -0300

    :sparkles: feat: adding the getCategory function using the axios, to set the category and pagination

---
+++ src/products/category.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const Api = `http://localhost:4000/product/category`
+// @ts-ignore
+export const getCategory = async (limit, offset, category) => {
+    return axios.get(`${Api}/${category}?limit=${limit}&offset=${offset}`).then((res) => {
+        let data = res.data
+        return data;
+    });
+}
+// @ts-ignore
+export const getCategoryById = async (id) => {
+    let apiId = `${Api}/${id}`
+    return axios.get(apiId).then((res) => {
+        let data = res.data
+        return data;
+    });
+}
