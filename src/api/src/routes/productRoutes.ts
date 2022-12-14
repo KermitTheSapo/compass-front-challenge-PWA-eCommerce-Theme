1d4cf246f823e556d1fdb4ced6b377fdb6945aba
Robert <rjsf06@gmail.com>
2022-12-14 16:26:08 -0300

    :sparkles: feat: adding the category path in the product Routes

--- src/api/src/routes/productRoutes.ts
+++ src/api/src/routes/productRoutes.ts
@@ -5,6 +5,7 @@ const router = express.Router()
 
 router
   .get('/', ProductController.getProduct)
+  .get("/category/:category", ProductController.getCategoryByName)
   .get('/:id', ProductController.getProductById)
   .post('/', ProductController.postProduct)
   .put('/:id', ProductController.putProduct)
