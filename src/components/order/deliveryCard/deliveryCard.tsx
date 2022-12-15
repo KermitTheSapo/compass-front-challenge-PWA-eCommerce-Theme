72fe5685d7938f46dc85e29851b0eb196a8eb24c
Robert <rjsf06@gmail.com>
2022-12-15 11:16:48 -0300

    :sparkles: feat: adding the date in the deliveryCard

--- src/components/order/deliveryCard/deliveryCard.tsx
+++ src/components/order/deliveryCard/deliveryCard.tsx
@@ -4,14 +4,15 @@ type Props = {
     name: string
     paragraph: string
     img: string
+    date: string
 }
 
-export default function DeliveryCard({ name, paragraph, img }: Props) {
+export default function DeliveryCard({ name, paragraph, img, date }: Props) {
     return (
         <S.DeliveryCardContainer>
             <S.ImgProduct src={img} alt="product image" />
             <S.ProductContent>
-                <S.ProductDate>08 Dec</S.ProductDate>
+                <S.ProductDate>{date}</S.ProductDate>
                 <S.ProductName>{name}</S.ProductName>
                 <S.ProductParagraph>{paragraph}</S.ProductParagraph>
             </S.ProductContent>
