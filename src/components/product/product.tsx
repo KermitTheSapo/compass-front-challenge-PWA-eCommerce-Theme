b5c85539bb9c409ff958f93fc33eb6a4b3a1fec8
Robert <rjsf06@gmail.com>
2022-12-15 11:20:10 -0300

    :sparkles: feat: passing the in to product page

--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -32,7 +32,7 @@ export default function Product() {
         <>
             {productsList && productsList.map((item, key) => (
                 <div key={key}>
-                    <ProductPage name={item.name} img={item.image} discount={item.discount} productDescription="Experience comfortable and easy traveling like never before with this coach bag. It features a zip closure, removable straps and multiple organization compartments to keep your valuables safe. Crafted from premium material, it is durable and lasts long." productParagraph={item.paragraph} safe={item.safe} value={item.price} imgCarousel={item.image} ratings={item.ratings} />
+                    <ProductPage name={item.name} img={item.image} discount={item.discount} productDescription="Experience comfortable and easy traveling like never before with this coach bag. It features a zip closure, removable straps and multiple organization compartments to keep your valuables safe. Crafted from premium material, it is durable and lasts long." productParagraph={item.paragraph} safe={item.safe} value={item.price} imgCarousel={item.image} ratings={item.ratings} _id={item._id}/>
                 </div>
             ))}
             <Helmet>
