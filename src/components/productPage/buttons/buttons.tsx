4e9dc801452d683f6c07e75d0efbf3d425210b1d
Robert <rjsf06@gmail.com>
2022-12-16 08:44:05 -0300

    :sparkles: feat: addToWishlsit function

--- src/components/productPage/buttons/buttons.tsx
+++ src/components/productPage/buttons/buttons.tsx
@@ -2,13 +2,14 @@ import * as S from "./buttonsStyle"
 
 type Props = {
     onClick: () => void;
+    wishlist: () => void;
 }
 
-export default function Buttons({ onClick }: Props) {
+export default function Buttons({ onClick, wishlist }: Props) {
     return (
         <S.ButtonsContainer>
             <S.ButtonAddBag onClick={onClick}>Add to bag</S.ButtonAddBag>
-            <S.ButtonAddWish>Add To Wishlist</S.ButtonAddWish>
+            <S.ButtonAddWish onClick={wishlist}>Add To Wishlist</S.ButtonAddWish>
         </S.ButtonsContainer>
     )
 }
