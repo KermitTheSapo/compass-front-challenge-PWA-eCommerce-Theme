f53afbfb57f950ac1983bacc00a43894f766be2a
Robert <rjsf06@gmail.com>
2022-12-16 08:49:13 -0300

    :sparkles: feat: adding the savedHeader

--- src/components/profile/userProfile/savedCards/savedCards.tsx
+++ src/components/profile/userProfile/savedCards/savedCards.tsx
@@ -1,16 +1,22 @@
 import { getOrder } from "../../../../products/order"
 import { useEffect, useState } from "react"
 import * as S from "./savedCardsStyle"
+import arrow from "@/assets/imgs/userProfile/arrowLeft.svg"
+import { useNavigate } from "react-router"
 
 export default function SavedCards() {
     const [upi, setUpi] = useState([])
+    const navigate = useNavigate()
     useEffect(() => {
         //@ts-ignore
         getOrder().then(res => setUpi(res.map((item) => item.upi)))
     }, [])
-    console.log(upi)
     return (
         <S.SavedCardsContainer>
+            <S.SavedHeader>
+                <S.ImgArrowLeft src={arrow} alt="left arrow" onClick={() => navigate(-1)} />
+                <S.SavedHeaderTitle>My Saved Cards</S.SavedHeaderTitle>
+            </S.SavedHeader>
             {
                 upi.length === 0 ? <S.Title>Error 404 - page not found</S.Title> : null
             }
