b037bfbc9de40d932fc34086984d1cca91827cd9
Robert <rjsf06@gmail.com>
2022-12-16 08:48:21 -0300

    :sparkles: feat: addToBag function button

--- src/components/profile/userProfile/orderDetail/cardOrder/cardOrder.tsx
+++ src/components/profile/userProfile/orderDetail/cardOrder/cardOrder.tsx
@@ -1,3 +1,4 @@
+import { postBag } from "../../../../../products/bag";
 import * as S from "./cardOrderStyle"
 
 type Props = {
@@ -6,9 +7,30 @@ type Props = {
     description: string;
     price: number;
     quantity: number;
+    safe: number;
+    discount: number;
+    ratings: number;
 }
 
-export default function CardOrder({image, name, description, price, quantity}: Props) {
+export default function CardOrder({ image, name, description, price, quantity, safe, discount, ratings }: Props) {
+    const addToBag = () => {
+        const Bag = {
+            name: name,
+            paragraph: description,
+            description: description,
+            price: price,
+            subTotal: price,
+            safe: safe,
+            discount: discount,
+            link: "Link",
+            imgAlt: "ImgAlt",
+            image: image,
+            ratings: ratings,
+            quantity: quantity
+        }
+        postBag(Bag)
+        alert("add to bag!")
+    }
     return (
         <S.CardOrderContainer>
             <S.ImgProduct src={image} alt="image of a product" />
@@ -17,7 +39,7 @@ export default function CardOrder({image, name, description, price, quantity}: P
                 <S.ProductDescription>{description}</S.ProductDescription>
                 <S.ProductPrice>${price}</S.ProductPrice>
                 <S.ProductQuantity>Qty- {quantity}</S.ProductQuantity>
-                <S.AddToBagBtn>Add to bag</S.AddToBagBtn>
+                <S.AddToBagBtn onClick={() => addToBag()}>Add to bag</S.AddToBagBtn>
             </S.DivContent>
         </S.CardOrderContainer>
     )
